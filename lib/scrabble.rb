c43b307282f769524fae38cfd0f014116abe197e
Anacelia Gillette <acgillette426@gmail.com>
2017-02-27 14:49:39 -0800

    Initial files made and tests

---
+++ lib/scrabble.rb
@@ -0,0 +1,19 @@
+module Scrabble
+
+  class Scoring
+
+    def initialize
+      @score_chart = {
+        ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"] => 1,
+        ["D", "G"] => 2,
+        ["B", "C", "M", "P"] => 3,
+        ["F", "H", "V", "W", "Y"] => 4,
+        ["K"] => 5,
+        ["J", "X"] => 8,
+        ["Q", "Z"] => 10
+      }
+    end
+
+  end
+
+end
---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require_relative '../lib/scrabble'
+
+describe "Baseline" do
+  before do
+    scrabble = Scrabble::Scoring.new
+  end
+
+  it "Creates an instance of the Scrabble class" do
+    scrabble.must_be_instance_of Scrabble::Scoring
+  end
+
+  it "Initializes with a hash"
+  skip
+  end
+
+  it "Iterates through the Hash key correctly" do
+    skip
+  end
+end

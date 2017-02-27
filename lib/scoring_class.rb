c379b4fa7260eec4e156170c758ca6d6443be32f
Olivia <ssecretsharer@gmail.com>
2017-02-27 15:55:08 -0800

    as of 4 pm

---
+++ lib/scoring_class.rb
@@ -0,0 +1,31 @@
+module Scrabble
+
+  class Scoring
+
+    SCORE_CHART = [{}]
+
+    def self.score(word)
+      #grab word from user, into array_of_words
+      #split word into characters array
+      #make new array of characters
+      #convert chars to INT using each_index?
+      #sum all numbers from letters in word
+      #stores in array of scores
+    end
+
+    def score_keeper
+      #grab score from scores array
+      #create an index of words played
+
+
+    end
+
+
+    def self.highest_score_from(array_of_words)
+      #.length or max_by to find
+      #longest word in array_of_words
+      #return longest word
+
+    end
+  end
+end

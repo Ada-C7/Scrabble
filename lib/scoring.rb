7ed97dd4a46895a539171c066a90cca9895e77a2
Brenna <bcmdarroch@gmail.com>
2017-02-27 15:14:38 -0800

    started first tests, discussed class methods/variables in Scoring

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,15 +1,18 @@
 module Scrabble
   class Scoring
-  # related to Play class, to be created
+# related to Play class, to be created
 
-# assign values to letters using hash like A: 1, B: 3...
+# class variable: assign values to letters using @@hash like A: 1, B: 3...
+
 # calculating one word's score
+# def self.score(word)
 # method to calculate & return score, given a word as an argument, call add_word method
 # if word length is 7, 50 bonus points
 
 # method to add a word and its score (as an array) to the @wordarray (from which high score will be calculated)
 
 # calculating highest score from @wordarray
+# def self.highest_score_from(array_of_words)
 # method to iterate through wordarray and return word with the highest score
 
 # if words are tied, tiebreaker method returns the shorter word
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,6 +1,19 @@
 describe "Scrabble::Scoring" do
 
+  before do
+    easy_word = Scrabble::Scoring.new("moo")
+    hard_word = Scrabble::Scoring.new("hipster")
+  end
+
+# test we can initalize Scoring
+describe "New Scoring object is instance of Scrabble::Scoring class" do
+  easy_word.must_be_instance_of Scrabble::Scoring
+end
 # test if letters have correct values attached
+describe "Letters have correct values" do
+
+end
+
 # test that word is scored correctly
 # also correct in case of: 7 letter word
 # score method returns correct score

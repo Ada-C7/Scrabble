35cb48d3de71fb958cc2d6a1b69eb7d105e124c9
sai samant <saisamant@gmail.com>
2017-02-27 15:31:24 -0800

    added psuedocode to score method and wrote out some tests for score

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,4 +1,15 @@
 module Scrabble
   class Scoring
+
+  def self.score word
+    #check if the word is a string of letters. if it isn't raise an Argument error
+    #iterate over the characters in the word and assign each letter a value from the score chart and add up the total
+    #if the string is 7 characters add 50 points to the total
+    #return total score
   end
+
+  end
+
+
+
 end
--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -6,18 +6,34 @@ require_relative '../lib/scrabble.rb'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "Scoring" do
-  it "Must return an integer" do
-  end
 
-  it "Must receive String input" do
+  describe "score" do
+    before do
+      @score = Scrabble::Scoring.score("word")
+    end
 
-  end
+    it "Must return an integer" do
+      @score.must_be_instance_of Integer
+    end
 
-  it "7-letter words must get 50 more points" do
+    it "Must receive String of letters as input" do
+      proc {
+        Scrabble::Scoring.score("5h7")
+      }.must_raise ArgumentError
 
-  end
+      proc {
+        Scrabble::Scoring.score("bats?")
+      }.must_raise ArgumentError
 
-  it "Must return correct score for word input" do
 
+    end
+
+    it "7-letter words must get 50 more points" do
+      
+    end
+
+    it "Must return correct score for word input" do
+
+    end
   end
 end

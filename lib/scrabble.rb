823ba609c840cf65f3a82318e97764952beff795
Bo Trethewey <botrethewey@gmail.com>
2017-02-27 15:44:37 -0800

    WIP: added more tests for self.score

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -12,8 +12,11 @@ module Scrabble
     }
 
     # Calculates and returns the total score for the given word
-    def self.score(word = nil)
+    def self.score(word)
+      raise ArgumentError.new("Invalid data type entered for word") if (word.class) != String
       word_score = 0
+
+      char_array = word.upcase.chars.to_a
       return word_score
     end
 
--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -7,9 +7,37 @@ Minitest::Reporters.use!
 Minitest::Reporters::SpecReporter.new
 
 describe "Scrabble Scoring" do
+
   describe "Self.score method tests" do
+
     it "Method can be called without an error" do
       Scrabble::Scoring.must_respond_to :score
     end
+
+    it "Raises an error when the word parameter is invalid data type" do
+      proc{
+        Scrabble::Scoring.score(-1033)
+      }.must_raise ArgumentError
+
+      proc{
+        Scrabble::Scoring.score([1,2,3])
+      }.must_raise ArgumentError
+    end
+
+    it "Returns the word score as a positive integer value" do
+      Scrabble::Scoring.score("").class.must_equal Integer
+      Scrabble::Scoring.score("").must_be :>=, 0
+      Scrabble::Scoring.score("").must_equal 0
+      Scrabble::Scoring.score("hat").must_equal 6
+
+    end
+
   end
+
+  describe "self.highest_score_from method tests" do
+    it "Method can be called without an error" do
+      Scrabble::Scoring.must_respond_to :highest_score_from
+    end
+  end
+
 end

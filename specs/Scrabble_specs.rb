57a064a91b15cb0cb35b7d625eb087595da563b7
Spatterjaaay <aja.pursova@gmail.com>
2017-02-27 14:52:47 -0800

    Created specs file

---
+++ specs/Scrabble_specs.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require_relative '../lib/scrabble'
+
+Minitest::Reporters.use!
+Minitest::Reporters::SpecReporter.new

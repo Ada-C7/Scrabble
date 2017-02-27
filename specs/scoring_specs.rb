cb5a6b2aafbc4c535600382f8832b42e1493c932
Theresa Manney <theresa.m.17@gmail.com>
2017-02-27 15:01:14 -0800

    Added test outline for self.score

--- specs/scoring_specs.rb
+++ specs/scoring_specs.rb
@@ -8,4 +8,35 @@ describe "Scoring is a class" do
     scoring = Scrabble::Scoring.new
     scoring.must_be_instance_of Scoring
   end
+
+  describe "self.score" do
+    it "Scoring knows-about self.score" do
+    end
+
+    it "Returns the total score for a given word" do
+    end
+
+    it "Raise ArgumentError if input is not a string" do
+    end
+
+    it "Seven letter word adds 50 point score bonus" do
+    end
+
+    it "Raise ArgumentError if input > 7 letter" do
+    end
+
+    it "Raise ArgumentError if empy string" do
+    end
+
+
+  end
+
+  describe "self.highest_score_from" do
+    it "Scoring knows-about self.highest_score_from" do
+    end
+
+    
+  end
+
+
 end

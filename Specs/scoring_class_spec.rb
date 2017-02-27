a3357a76eb7d4503ab33420ec195864035aa11f9
ashtn <ahstonehorton@gmail.com>
2017-02-27 15:36:41 -0800

    Added 3 spec stubs for Score Method

--- Specs/scoring_class_spec.rb
+++ Specs/scoring_class_spec.rb
@@ -2,3 +2,19 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/scoring_class'
+
+describe "Score Metihd for Scoring Class" do
+
+  it "Score Method outputs integer" do
+
+  end 
+
+  it "Score method will total the value of each letter in the word" do
+
+  end
+
+  it "7 letter word adds 50 points to total score" do
+
+  end
+
+end

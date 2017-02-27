9a6f2331bf01d6cbff96201fae1b7d548f71f0be
Regan Huff <regan.huff@gmail.com>
2017-02-27 15:44:31 -0800

    Add new tests to correct file

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -12,4 +12,47 @@ describe "Wave 1" do
     end
 
   end
+
+  require 'minitest/autorun'
+  require 'minitest/reporters'
+  require 'minitest/skip_dsl'
+
+  require_relative '../lib/scoring.rb'
+
+
+    describe "self.score method" do
+      # not yet a functional test
+      it "accepts a word as input" do
+        Scrabble::Scoring.score
+      end
+
+      it "stores the word as an array of characters" do
+        Scrabble::Scoring.score("frog")
+
+      end
+
+      it "converts the char array to a symbol array" do
+
+      end
+
+      it "successfully retrieves score for one of the word's letters" do
+
+      end
+
+      it "returns correct score" do
+
+      end
+
+      it "correctly adds bonus for seven-letter word" do
+
+      end
+
+
+
+
+    end
+
+
+  end
+
 end

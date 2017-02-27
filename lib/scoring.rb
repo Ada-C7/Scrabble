9298766378b380fa7bee2ddda577bf64b1e09bee
Brenna <bcmdarroch@gmail.com>
2017-02-27 15:04:07 -0800

    created plan for code and tests

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,4 +1,23 @@
 module Scrabble
   class Scoring
+  # related to Play class, to be created
+
+# assign values to letters using hash like A: 1, B: 3...
+# calculating one word's score
+# method to calculate & return score, given a word as an argument, call add_word method
+# if word length is 7, 50 bonus points
+
+# method to add a word and its score (as an array) to the @wordarray (from which high score will be calculated)
+
+# calculating highest score from @wordarray
+# method to iterate through wordarray and return word with the highest score
+
+# if words are tied, tiebreaker method returns the shorter word
+# if multiple words are same length and score, just pick first word
+
+
+
+
+
   end
 end
---
+++ specs/scoring_spec.rb
@@ -0,0 +1,13 @@
+describe "Scrabble::Scoring" do
+
+# test if letters have correct values attached
+# test that word is scored correctly
+# also correct in case of: 7 letter word
+# score method returns correct score
+# word and score are pushed to @wordarray
+# test that highest score method calculates correctly
+# test that highest score is returned
+# test that highest score handles ties
+
+
+end

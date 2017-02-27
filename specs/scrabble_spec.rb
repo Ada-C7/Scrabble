30cce5f3a874e6ae59238a49f7b60a8f2073c8e0
sai samant <saisamant@gmail.com>
2017-02-27 14:55:13 -0800

    added basic structure to both files

---
+++ specs/scrabble_spec.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require_relative '../lib/scrabble.rb'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+describe "Scoring" do
+  it "" do
+    
+  end
+
+end

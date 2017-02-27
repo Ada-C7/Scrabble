88d5fa668062b71e16058353bf35e7d10c665bda
jjousun <jjousun@gmail.com>
2017-02-27 15:45:54 -0800

    Added tests for scoring adding-up

--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -29,11 +29,22 @@ describe "Scoring" do
     end
 
     it "7-letter words must get 50 more points" do
-      
+      @score_check = Scrabble::Scoring.score("penguin")
+      @score_check.must_equal 60 #10 points plus 50 bonus
+
+      @score_check = Scrabble::Scoring.score("chicken")
+      @score_check.must_equal 68 #10 points plus 50 bonus
     end
 
     it "Must return correct score for word input" do
+      @score_check = Scrabble::Scoring.score("falcon")
+      @score_check.must_equal 11
 
+      @score_check = Scrabble::Scoring.score("jay")
+      @score_check.must_equal 13
+
+      @score_check = Scrabble::Scoring.score("quail")
+      @score_check.must_equal 14
     end
   end
 end

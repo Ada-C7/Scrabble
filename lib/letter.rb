415c6429a3c65303fabe35802d72e33dc77e8b62
janicewilson <janice.wilson@mac.com>
2017-02-27 15:30:16 -0800

    Reorg.

---
+++ lib/letter.rb
@@ -0,0 +1,23 @@
+module Scrabble
+
+     class Letter
+
+          attr_reader :name
+
+          SCORING_CHART = { %w(A E I O U L N R S T) => 1, %w(D G) =>  2, %w(B C M P) =>  3, %w(F H  V W Y) =>  4, %w(K) =>  5, %w(J X) => 8, %w(Q z) => 10 }
+
+          def initialize(name)
+
+               @name = name
+               @point = assign_points
+          end
+
+
+          def assign_points
+
+               
+
+          end
+     end
+
+end

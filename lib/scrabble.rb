82196be035d119daa6add7517134d04befb1775b
Bo Trethewey <botrethewey@gmail.com>
2017-02-27 14:54:56 -0800

    WIP: Added self.score method

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,5 +1,22 @@
 module Scrabble
   class Scoring
+    # Class constant stores the letters and their repective scores
+    SCORE_CHART = {
+      1 => %w(A E I O U L N R S T),
+      2 => %W(D G),
+      3 => %W(B C M P),
+      4 => %W(F H V W Y),
+      5 => %W(K),
+      8 => %W(J X),
+      10 => %W(Q Z),
+    }
+
+    # Calculates and returns the total score for the given word
+    def self.score word
+      word_score = 0
+      return word_score
+    end
+
 
   end
 end

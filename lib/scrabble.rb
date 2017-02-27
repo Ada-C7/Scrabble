8c400244ad17d169cf5ccd562e3d767b8d0738dc
Haby Randall <sow.haby@gmail.com>
2017-02-27 15:59:43 -0800

    self.score method tests failing

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,22 +1,55 @@
 module Scrabble
 
   class Scoring
+    attr_accessor :word_array, :word
 
     def initialize(word)
-      word_array = word.split("")
+      # @word_array = word_array
+
     end
 
 
     SCORE_CHART = {
-      ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T",] => 1,
-      ["D", "G"] => 2,
-      ["B", "C", "M", "P"] => 3,
-      ["F", "H", "V", "W", "Y"] => 4,
-      ["K"] => 5,
-      ["J", "K"] => 8,
-      ["Q", "Z"] => 10
+      "A" => 1,
+      "E" => 1,
+      "I" => 1,
+      "O" => 1,
+      "U" => 1,
+      "L" => 1,
+      "N" => 1,
+      "R" => 1,
+      "S" => 1,
+      "T" => 1,
+      "D" => 2,
+      "G" => 2,
+      "B" => 3,
+      "C" => 3,
+      "M" => 3,
+      "P" => 3,
+      "F" => 4,
+      "H" => 5,
+      "V" => 5,
+      "W" => 5,
+      "Y" => 5,
+      "K" => 5,
+      "J" => 8,
+      "Q" => 10,
+      "Z" => 10
     }
 
-  end
 
+    def self.score(word)
+      score_total = 0
+      word_array.upcase = word.split("")
+      word_array.each do |letter|
+        score_total += SCORE_CHART[letter]
+      end
+      return score_total
+
+    end
+
+    def self.highest_score_from(array_of_words)
+    end
+
+  end
 end

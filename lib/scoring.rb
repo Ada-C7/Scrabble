aafc0cf754b3fdb3a9139904913d36332105a265
Jackie Watanabe <sushiigirl@gmail.com>
2017-02-27 14:39:53 -0800

    created initial structure for scoring.rb

---
+++ lib/scoring.rb
@@ -0,0 +1,16 @@
+module Scrabble
+  class Scoring
+
+
+  end
+
+
+end
+
+
+
+    #
+    # Create a Scrabble module at the project root.
+    # Create a Scrabble::Scoring class which contains some sort of data structure to store the individual letter scores listed below.
+    # Create a Spec file which corresponds to your Scrabble::Scoring class. This spec should contain one red test as a starting point (this test can be modified as your get further through the requirements).
+    # Be able to execute your one test using rake from the project root.

53d9c54b7f448d445144e5027a655e5c34ebc6c5
jjousun <jjousun@gmail.com>
2017-02-27 15:05:59 -0800

    Wrote descriptions for specs

--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -6,8 +6,18 @@ require_relative '../lib/scrabble.rb'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "Scoring" do
-  it "" do
-    
+  it "Must return an integer" do
   end
 
+  it "Must receive String input" do
+
+  end
+
+  it "7-letter words must get 50 more points" do
+
+  end
+
+  it "Must return correct score for word input" do
+
+  end
 end

f8a21109f0005dfe21c7c4190a00e174c515ddbe
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-02-28 13:36:46 -0800

    made letter scores a constant

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,11 +1,20 @@
 module Scrabble
   class Scoring
-    attr_reader :letter_scores
-    def initialize
-      @letter_scores = {a: 1, c: 3, b: 3, e: 1, d: 2, g: 2, f: 4, i: 1, h: 4, k: 5, j: 8,
-                      m: 3, l: 1, o: 1, n: 1, q: 10, p: 3, s: 1, r: 1, u: 1, t: 1, w: 4,
-                      v: 4, y: 4, x: 8, z: 10 }
-    end
+    LETTER_SCORES = { a: 1, c: 3, b: 3, e: 1, d: 2, g: 2, f: 4, i: 1, h: 4, k: 5, j: 8,
+                    m: 3, l: 1, o: 1, n: 1, q: 10, p: 3, s: 1, r: 1, u: 1, t: 1, w: 4,
+                    v: 4, y: 4, x: 8, z: 10 }
+    LETTER_SCORES = {"a" => 1, "c" => 3, "b" => 3, "e" => 1, "d" => 2, "g" => 2,
+             "f" => 4, "i" => 1, "h" => 4, "k" => 5, "j" => 8, "m" => 3,
+             "l" => 1, "o" => 1, "n" => 1, "q" => 10, "p" => 3, "s" => 1,
+             "r" => 1, "u" => 1, "t" => 1, "w" => 4, "v" => 4, "y" => 4,
+             "x" => 8, "z" => 10}
+
+    # attr_reader :letter_scores
+    #
+    # # refractor this to a constant - go back to strings?
+    # def initialize
+    #
+    # end
 
     def self.score(word)
       unless word.class == String

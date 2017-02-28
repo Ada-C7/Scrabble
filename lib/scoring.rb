d98fb1f28f2660bf0b33ad4db580556d6ac04065
Tamiko Terada <tamikodevo@gmail.com>
2017-02-27 17:32:03 -0800

    Scoring.score method updated and pass tests

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,52 +1,62 @@
 module Scrabble
-    class Scoring
-        attr_reader :word_score
-        SCORE_CHART = {
-            1 => %w(A E I O U L N R S T),
-            2 => %w(D G),
-            3 => %w(B C M P),
-            4 => %w(F H V W Y),
-            5 => ['K'],
-            8 => %w(J X),
-            10 => %w(Q Z)
-        }.freeze
+  class Scoring
+    attr_reader :word_score
+    SCORE_CHART = {
+      1 => %w(A E I O U L N R S T),
+      2 => %w(D G),
+      3 => %w(B C M P),
+      4 => %w(F H V W Y),
+      5 => %w(K),
+      8 => %w(J X),
+      10 => %w(Q Z)
+    }.freeze
 
-        def self.score(word)
-            letter_score = 0
-            @word_score = 0
-            word.each_char do |letter|
-                SCORE_CHART.each do |number, alpha|
-                    letter_score = number if alpha.include?(letter.upcase)
-                    @word_score += letter_score
-                end
-            end
-            @word_score
-        end
+    def self.score(word)
+      # starting word_score is based on word.length
+      if word.length < 7 && word.length > 0
+        @word_score = 0
+      elsif word.length == 7
+        @word_score = 50
+      else
+        raise ArgumentError.new "invalid word length"
+      end
 
-        def self.highest_score_from(array_of_words)
-            @all_scores = []
-            @highest_word = ''
-            @highest_score = 0
-            array_of_words.each do |word|
-                @all_scores << score(word)
-            end
-            @scored_words = Hash[@all_scores.zip(array_of_words.zip)]
-            @scored_words.each do |score, word|
-                if score > @highest_score
-                    @highest_word = word
-                    @highest_score = score
-                elsif score = @highest_score
-                    if word.length > @highest_word.length
-                        @highest_word = word
-                        @highest_score = score
-                        unless @highest_word.length == 7
-                        end
-                    end
-                end
-            end
-            @highest_word
+      word.each_char do |letter|
+        @letter = letter.upcase
+        SCORE_CHART.each do |number, array|
+          if array.include?(@letter)
+            @word_score += number
           end
         end
       end
+      return @word_score
+    end
 
-puts Scrabble::Scoring.highest_score_from(%w(words tofu))
+    def self.highest_score_from(array_of_words)
+      @all_scores = []
+      @highest_word = ''
+      @highest_score = 0
+      array_of_words.each do |word|
+        @all_scores << score(word)
+      end
+      @scored_words = Hash[@all_scores.zip(array_of_words.zip)]
+      @scored_words.each do |score, word|
+        if score > @highest_score
+          @highest_word = word
+          @highest_score = score
+        elsif score = @highest_score
+          if word.length > @highest_word.length
+            @highest_word = word
+            @highest_score = score
+            unless @highest_word.length == 7
+            end
+          end
+        end
+      end
+      @highest_word
+    end
+  end
+end
+
+# puts Scrabble::Scoring.score("squeeze")
+#puts Scrabble::Scoring.highest_score_from(%w(words tofu))

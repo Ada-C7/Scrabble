30fd18cf7cb8c9a4438102aa008c69f82d67ed71
Sofia Kim <sofiamhkim@gmail.com>
2017-02-28 14:40:54 -0800

    switching the role

--- lib/score.rb
+++ lib/score.rb
@@ -2,27 +2,28 @@ module Scrabble
   class Scoring
 
     DICTIONARY = nil
-    SCORE_CHART = nil
+    SCORE_CHART =
+    ["A1", "B3", "C3", "D2", "E1", "F4", "G2", "H4", "I1", "J8", "K5", "L1", "M3", "N1", "O1", "P3", "Q10", "R1", "S1", "T1", "U1", "V4", "W4", "X8", "Y4", "Z10"]
 
-    def is_a_word?(word)
-      # if the entered word is valid, we enter the score. Otherwise, we return 0
-    end
+    # def is_a_word?(word)
+    #   # if the entered word is valid", " we enter the score. Otherwise", " we return 0
+    # end
 
-    def calculating_score(word)
-      word.split("")
-      # look up each letter from score chart, get the total point value
-      #if the word uses all the player's tiles. At the 8th tile, the bonus will be given.
+    def calculating_letter(letter)
+      
+      # look up each letter from score chart", " get the total point value
+      #if the word uses all the player's tiles. At the 8th tile", " the bonus will be given.
     end
 
     def self.score(word)
-      if is_a_word?(word)
+      if word.split("")
         return score(word)
       else
         return 0
       end
     end
 
-    def break_ties(word1, word2)
+    def break_ties(word1", " word2)
       if word1.length > word2.length
         return word2
       else
@@ -33,7 +34,7 @@ module Scrabble
     def self.highest_score_from(array_of_words)
       #best_words = collections of highest scoring words pick the first one
       if best_words.length > 1
-        best_words == break_ties
+        best_words = break_ties(word1", "word2)
       else
         return best_words.join(break_ties)
       end
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -12,7 +12,7 @@ describe "class Scoring" do
 
   end
 
-  describe "is_a_word?" do
+  describe "#is_a_word?" do
     # it exists
     # it returns true or false
     # it returns true if the word is in DICTIONARY
@@ -21,8 +21,24 @@ describe "class Scoring" do
   end
 
   describe calculating_score do
-    # it returns 0 if the word does not exist
+
     # it returns the right point value for word that is less than 8 letters
     # it adds 50 points if the word is 8 letters
+  end
+
+  describe "self.score" do
+    # it returns the score of the word if is_a_word is true
+    # it returns 0 if the word does not exist
+  end
+
+  describe "break_ties" do
+    # it takes 2 words as params
+    # it compares the length of two words if word1 is bigger than word2, it returns word1 else, word2.
+  end
+
+  describe "self.highest_score_from" do
+    # it tests if there is a single word, it will be returned as the best word.
+    # it returns the shortest word as the best word if there is more than one best word
+    # if returns the first word if there are more than one word with same length.
   end
 end

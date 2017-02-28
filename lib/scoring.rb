2475d81141ad557b8bd39efd5a95f4fc1c176022
Emma Claire Philippides <acme9898@gmail.com>
2017-02-28 14:45:26 -0800

    first score test

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -3,14 +3,20 @@ require 'csv'
 module Scrabble
 
   class Scoring
-    attr_reader :score_chart
-    def initialize
-      @score_chart = []
-      CSV.open("support/score_chart.csv").each do |line|
-        @score_chart << line[1..-1]
-      end
-    end
-
+    SCORE_CHART = {'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1,
+      'N' => 1, 'R' => 1,  'S' => 1, 'T'=> 1,
+      'D' => 2, 'G' => 2,
+      'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3,
+      'F' => 4, 'H' => 4, 'V' => 4, 'W' => 4, 'Y' => 4,
+      'K' => 5,
+      'J' => 8, 'X' => 8,
+      'Q' => 10, 'Z' => 10 }
+      # attr_reader :score_chart
+      # def initialize
+      #   @score_chart = []
+      #   CSV.open("support/score_chart.csv").each do |line|
+      #     @score_chart << line[1..-1]
+      #   end
 
     def self.score
     end
@@ -20,5 +26,5 @@ module Scrabble
   end
 end
 
-letter = Scrabble::Scoring.new
-print letter.score_chart
+# letter = Scrabble::Scoring.new
+# print letter.score_chart
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -3,10 +3,53 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/scoring.rb'
 
-describe "scoring" do
-  it "Can be initialized" do
-    scoring = Scrabble::Scoring.new
-    scoring.must_be_instance_of Scrabble::Scoring
 
+describe "Scoring" do
+  describe "score" do
+    # let (:scoring) {Scrabble::Scoring.new}
+    # it "Can be initialized" do
+    #   # scoring = Scrabble::Scoring.new
+    #   scoring.must_be_instance_of Scrabble::Scoring
+    # end
+    it "returns an integer" do
+      score = Scrabble::Scoring.score("a")
+      score.must_be_instance_of Integer
+
+    end
+    it "returns a scrabble score for any given letter" do
+      Scrabble::Scoring.score("a").must_equal 1
+      Scrabble::Scoring.score("f").must_equal 4
+
+    end
+
+    it "returns a scrabble score of a word" do
+
+    end
   end
 end
+
+# define_method(:scrabble_score) do
+# 		scrabble_word = self.split("")
+# 		scrabble_score = 0
+#
+# 		scrabble_word.each() do |letter|
+# 			case letter
+# 			when "a", "A", "e", "E", "i", "I", "o", "O", "u", "U", "l", "L", "n", "N", "r", "R", "s", "S", "t", "T"
+# 				scrabble_score = scrabble_score.+(1)
+# 			when "d", "D", "g", "G"
+# 				scrabble_score = scrabble_score.+(2)
+# 			when "b", "B", "c", "C", "m", "M", "p", "P"
+# 				scrabble_score = scrabble_score.+(3)
+# 			when "f", "F", "h", "H", "v", "V", "w", "W", "y", "Y"
+# 				scrabble_score = scrabble_score.+(4)
+# 			when "k", "K"
+# 				scrabble_score = scrabble_score.+(5)
+# 			when "j", "J", "x", "X"
+# 				scrabble_score = scrabble_score.+(8)
+# 			when "q", "Q", "z", "Z"
+# 				scrabble_score = scrabble_score.+(10)
+# 			else
+# 				scrabble_score
+# 			end
+# 		end
+# 		scrabble_score

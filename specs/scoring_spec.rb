694f03ed7152c1c21e270b4b6c4fd71482898670
Danielle Birbal <birbalds@gmail.com>
2017-02-27 16:07:16 -0800

    Added testing for self.score()

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -2,3 +2,23 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/scoring'
+
+describe Scrabble::Scoring do
+    describe 'self.score(word)' do
+        it 'Returns an integer' do
+            Scrabble::Scoring.score('word').must_be_instance_of Integer
+        end
+
+        it 'Returns the correct score' do
+            Scrabble::Scoring.score('word').must_equal 8
+        end
+
+        it 'Argument is case insensitive' do
+            Scrabble::Scoring.score('wOrD').must_equal 8
+        end
+
+        it 'A seven letter word results in 50 point bonus' do
+            Scrabble::Scoring.score('zizzled').must_equal 85
+        end
+    end
+end

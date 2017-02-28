31a7c1c79bdcb6832ceb16ebe04edf3428e9cc24
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-02-28 14:43:27 -0800

    Have highest_score_from method and tests working - include notes in comments

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -16,6 +16,17 @@ module Scrabble
     #
     # end
 
+    def self.tie_breaker(tie_words)
+      tie_words.each do |word|
+        if word.length == 7
+          return word
+        else
+          return tie_words.min_by(&:length)
+        end
+      end
+      #  unless tie_words.select { |word| word.length == 7 }
+    end
+
     def self.score(word)
       unless word.class == String
         raise ArgumentError.new "Input word must be a string"
@@ -24,5 +35,22 @@ module Scrabble
       word_score = LETTER_SCORES.values_at(*word.chars).reduce(:+)
       word.length == 7 ? word_score += 50 : word_score
     end
+
+    def self.highest_score_from(words)
+      raise ArgumentError if words.class != Array
+      # will return a hash where key is the score and value is array of the word(s)
+      # with that score
+      scores_and_words = words.group_by { |word| score(word) }
+      # now we need to get the word we want out of the scores hash
+      # we want to return the value of the higest key
+      # this returns an array with the score at idex zero and word list at index 1
+      highest_words = scores_and_words.max_by { |score, word_list| score }
+      highest_words[1].length > 1 ? tie_breaker(highest_words[1]) : highest_words[1].join
+    end
   end
 end
+
+# print Scrabble::Scoring.highest_score_from(["ox", "meow", "cat", "ax"])
+# puts
+# print Scrabble::Scoring.highest_score_from( ["algebra", "qzqzqz"])
+# puts
--- spec/scoring_spec.rb
+++ spec/scoring_spec.rb
@@ -66,8 +66,44 @@ describe "Scoring" do
     end
 
     it "adds 50 point bonus for 7 letter words" do
-      Scrabble::Scoring.score("algebra").must_equal 60 
+      Scrabble::Scoring.score("algebra").must_equal 60
     end
 
+  end
+
+  describe "Scoring#highest_score_from" do
+
+    before do
+      @words_array = ["cat", "hello", "algebra"]
+      @tie_words = ["ox", "meow", "cat", "ax"]
+      @tie_words2 = ["algebra", "qzqzqz"]
+      # @seven_letter_words = ["abashes", "abettal", "absents", "battler", "beanies", "beavers", "calling"]
+    end
+
+      it "raises an argument error is not given an array" do
+        proc {
+          Scrabble::Scoring.highest_score_from("cat")
+        }.must_raise ArgumentError
+      end
+
+      it "returns a string" do
+        Scrabble::Scoring.highest_score_from(@words_array).must_be_kind_of String
+      end
+
+      it "returns the word with the higest score" do
+        Scrabble::Scoring.highest_score_from(@words_array).must_equal "algebra"
+      end
+
+      it "in tie, returns the word with the fewest letters" do
+        Scrabble::Scoring.highest_score_from(@tie_words).must_equal "ox"
+      end
+
+      it "in tie, returns 7 letter words before others" do
+        Scrabble::Scoring.highest_score_from(@tie_words2).must_equal "algebra"
+      end
+
+      it "in a tie with same length, returns the first one" do
+        Scrabble::Scoring.highest_score_from(@tie_words).must_equal "ox"
+      end
   end
 end

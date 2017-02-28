fe201c3ffe712279b5d63249deeebb30a895c2f4
Hyunji Kim <ricecakemonster@gmail.com>
2017-02-27 16:17:45 -0800

    Added Letters

--- lib/scrabble_scoring.rb
+++ lib/scrabble_scoring.rb
@@ -1,5 +1,13 @@
 module Scrabble
   class Scoring
-
+    LETTERS = [
+              {1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]},
+              {2 => ["D","G"]},
+              {3 => ["B", "C", "M", "P"]},
+              {4 => ["F","H","V","W","Y"]},
+              {5 => ["K"]},
+              {8 => ["J", "X"]},
+              {10 => ["Q", "Z"]}
+            ]
   end
-end 
+end

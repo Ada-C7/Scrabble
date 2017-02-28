e3788e67a53b43796a44f8397af2bbbd3b5862f9
Andrea Valliere <amvalliere@gmail.com>
2017-02-28 14:33:27 -0800

    Added test descriptions, score chart constant, and score class method skeleton.

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,4 +1,32 @@
 module Scrabble
  class Scoring
+   SCORE_CHART = {
+     1 => [
+       "A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
+     ],
+     2 => [
+       "D", "G"
+     ],
+     3 => [
+       "B", "C", "M", "P"
+     ],
+     4 => [
+       "F", "H", "V", "W", "Y"
+     ],
+     5 => [
+       "K"
+     ],
+     8 => [
+       "J", "X"
+     ],
+     10 => [
+       "Q", "Z"
+     ]
+   }
+
+   def self.score(word)
+     
+   end
+
  end
 end
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -4,23 +4,46 @@ require 'minitest/skip_dsl'
 require_relative '../lib/scoring'
 
 describe "Scoring class" do
-
   it "Creates a new instance of Scoring class" do
     score = Scrabble::Scoring.new
     score.must_be_instance_of Scrabble::Scoring
   end
 
-  it "Compares word to score chart by each letter" do
+  describe "self.score method" do
+    it "Compares word to score chart by each letter" do
+      word = Scrabble::Scoring.score("ruby")
+      word.must_equal 9
+    end
 
+    it "Adds up the score" do
+
+    end
+
+    it "Adds 50 bonus points for a seven-letter word" do
+
+    end
   end
 
-  it "Adds up the score" do
+  describe "self.highest_score_from(array_of_words)" do
+    it "loop through and find highest scoring word" do
+
+    end
+
+    it "in tie, fewest letters wins" do
+
+    end
+
+    it "7 letter word wins in a tie" do
+
+    end
+
+    it "7 letter word gets 50 point bonus" do
+
+    end
+
+    it "multiple words of same score and same length, first one is returned" do
+
+    end
 
   end
-
-  it "Adds 50 bonus points for a seven-letter word" do
-    
-  end
-
-
 end

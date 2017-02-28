ea3d28056fb77dec52fdfcab6670e2b4209a9eca
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-02-28 14:42:11 -0800

    Writing test for #won?

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,18 @@
 module Scrabble
   class Player
+
+    def won?
+    total_score  > 100
+    end
+
+    def highest_scoring_word(words)
+
+    end
+
+    def highest_word_score
+
+    end
+
+
   end
 end

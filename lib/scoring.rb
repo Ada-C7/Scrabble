be96ca2f78872c2370e03f242fae7d5e44d15998
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-02-28 13:47:56 -0800

    fixed indentation

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,7 +1,7 @@
 module Scrabble
   class Scoring
-SCORES = {1 => %w(A E I O U L N R S T), 2=> %w( D.G),   3=> w%(B C M P), 4 => %w(F H V W Y), 5 => ["K"], 8=> %w(J X) , 10=> w%(Q Z)}
-def self.score
-end
+    SCORES = {1 => %w(A E I O U L N R S T), 2=> %w( D.G),   3=> w%(B C M P), 4 => %w(F H V W Y), 5 => ["K"], 8=> %w(J X) , 10=> w%(Q Z)}
+    def self.score
+    end
   end
 end

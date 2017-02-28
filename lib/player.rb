70b4c1725268290a54872f251c39f23b72b96272
Alix Hamilton <alix.hamilton@gmail.com>
2017-02-28 14:40:57 -0800

    adds initial player methods

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,28 @@
 module Scrabble
   class Player
+    attr_reader :name, :plays
+
+    def initialize(name)
+      @name = name
+      @plays = []
+    end
+
+    def play(word)
+      return false if won?
+      @plays << word
+      Scoring.score(word)
+    end
+
+    def total_score
+      @plays.map { |word| Scoring.score(word) }.reduce(0, :+)
+    end
+
+    def won?
+      total_score > 100
+    end
+
+    def highest_scoring_word
+      @plays.max_by { |word| Scoring.score(word) }
+    end
   end
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -2,3 +2,46 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative "../lib/player"
+
+describe Scrabble::Player do
+  before do
+    @alix = Scrabble::Player.new("Alix")
+  end
+  describe "#initialize" do
+    it "Initializes with a player's name" do
+      @alix.name.must_equal "Alix"
+    end
+
+    it "Does not initialize without player name" do
+      proc {
+        Scrabble::Player.new
+      }.must_raise ArgumentError
+    end
+  end
+
+  describe "#play" do
+    it "Adds a played word to the plays" do
+      @alix.play("pickle")
+      @alix.play("walrus")
+      @alix.plays.must_equal ["pickle", "walrus"]
+    end
+
+    it "Returns false if the player has won" do
+      @alix.play("pizzazz")
+      @alix.play("quiz")
+      @alix.play("blanket").must_equal false
+    end
+  end
+
+  describe "#total_score" do
+    it "Returns the player's total score" do
+      @alix.play("pizzazz") # 95 points
+      @alix.play("quiz") # 22 points
+      @alix.total_score.must_equal 117
+    end
+
+    it "Returns a score of 0 if no words have been played" do
+      @alix.total_score.must_equal 0
+    end
+  end
+end

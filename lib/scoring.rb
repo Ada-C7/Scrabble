f4307d2feec335df8504e1ba0b2cd942adb2d963
Emma Claire Philippides <acme9898@gmail.com>
2017-02-27 16:42:58 -0800

    initialize

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,14 +1,24 @@
+require 'csv'
+
 module Scrabble
 
   class Scoring
-    # def initialize
-    # end
-    SCORE_CHART = [[""]]
+    attr_reader :score_chart
+    def initialize
+      @score_chart = []
+      CSV.open("support/score_chart.csv").each do |line|
+        @score_chart << line[1..-1]
+      end
+    end
 
 
     def self.score
     end
 
     def self.highest_score_from(array_of_words)
+    end
   end
 end
+
+letter = Scrabble::Scoring.new
+print letter.score_chart

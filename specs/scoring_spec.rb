4426a5813187a528c78ff2fbbe87dba3c4244bb0
EricaJCase <ericajcase@gmail.com>
2017-02-28 13:43:42 -0800

    Added test pseudocode for is_a_word? and calculating_score

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -3,3 +3,26 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative '../lib/score.rb'
+
+describe "class Scoring" do
+  it "reads in DICTIONARY" do
+  end
+
+  it "reads in SCORES" do
+
+  end
+
+  describe "is_a_word?" do
+    # it exists
+    # it returns true or false
+    # it returns true if the word is in DICTIONARY
+    #it returs false  if the word is not in DICTIONARY
+
+  end
+
+  describe calculating_score do
+    # it returns 0 if the word does not exist
+    # it returns the right point value for word that is less than 8 letters
+    # it adds 50 points if the word is 8 letters
+  end
+end

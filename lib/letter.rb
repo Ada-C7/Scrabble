f149309f3566f0007c6a7323697ce2a54b105fa5
Spatterjaaay <aja.pursova@gmail.com>
2017-02-27 16:02:57 -0800

    Accidental change

--- lib/letter.rb
+++ lib/letter.rb
@@ -7,7 +7,6 @@ module Scrabble
           SCORING_CHART = { %w(A E I O U L N R S T) => 1, %w(D G) =>  2, %w(B C M P) =>  3, %w(F H  V W Y) =>  4, %w(K) =>  5, %w(J X) => 8, %w(Q z) => 10 }
 
           def initialize(name)
-
                @name = name
                @point = assign_points
           end
@@ -15,7 +14,7 @@ module Scrabble
 
           def assign_points
 
-               
+
 
           end
      end

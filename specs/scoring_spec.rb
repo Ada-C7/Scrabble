65d0fd509bf1df01893360ce5dfbf49b7df67d54
Ting Wong <hellotingwong@gmail.com>
2017-02-28 14:01:24 -0800

    updated spec files and started scoring file

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -9,21 +9,40 @@ Minitest::Reporters.use!
 Minitest::Reporters::SpecReporter.new
 
 describe "Wave 1" do
-  describe "initialize" do
-
-# 	it "instantiates that the word is in our array" do
-# 	word.must_be_instance_of
-# 	end
-
+  describe "self.score(word)" do
     it "verifies that a numeric score is returned" do
-      word = Scrabble::Scoring.score("potato")
-      word.must_be_kind_of Enumerable
+      score = Scrabble::Scoring.score("potato")
+      score.must_be_kind_of Integer
     end
 
-    it "verifies there are max 7 letters" do
+    it "returns the total score of the given word" do
 
     end
 
+    it "applies the bonus if there are 7 letters" do
 
+    end
+
+    it "provides the total score including bonus if there are 7 letters" do
+
+    end
+  end
+
+  describe "self.highest_score_from(array)" do
+    it "returns the word in the array with the highest score" do
+
+    end
+
+    it "if tied, prefers the word with the fewest letters" do
+
+    end
+
+    it "if multiple words, chooses the one with 7 letters with bonus" do
+
+    end
+
+    it "if multiple words of same score and length, choose one in supplied list" do
+
+    end
   end
 end

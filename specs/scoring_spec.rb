ae0c4b9d62d7ee62818a2b5093c53cdaa6432ab3
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-02-27 16:30:51 -0800

    created files and dirs

---
+++ specs/scoring_spec.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require 'minitest/pride'
+require_relative '../lib/scoring'
+
+describe "Module Scrabble, Scoring Class" do
+  it "exists" do
+    skip
+    
+  end
+end

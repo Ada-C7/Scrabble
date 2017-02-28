4f0647b542f779fa82ad308041860611ae2cd727
tehut <tehut.m.g@gmail.com>
2017-02-27 16:39:46 -0800

    test highest_word method

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -7,6 +7,10 @@ require_relative '../lib/scoring'
 describe "Module Scrabble, Scoring Class" do
   it "exists" do
     skip
-    
+
+  end
+
+  it "self.highest_word returns string"do
+  self.highest_word.must_be_kind_of String
   end
 end

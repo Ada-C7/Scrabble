ad0f8c29bb2351a83d2ca4fc64e14f31568ce7ac
Ting Wong <hellotingwong@gmail.com>
2017-02-28 14:29:03 -0800

    added quotes to SCORE constant

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,30 +1,28 @@
 module Scrabble
-  SCORING = [
-    {1 => [A, E, I, O, U, L, N, R, S, T]},
-    {2 => [D, G]},
-    {3 =>[B, C, M, P]},
-    {4 =>[F, H, V, W, Y]},
-    {5 => [K]},
-    {8 => [J, X]},
-    {10 => [Q, Z]}
-  ]
   class Scoring
-    attr_reader :word_array
-    # def initialize
-    # end
+    SCORE = [
+      {1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]},
+      {2 => ["D", "G"]},
+      {3 =>["B", "C", "M", "P"]},
+      {4 =>["F", "H", "V", "W", "Y"]},
+      {5 => ["K"]},
+      {8 => ["J", "X"]},
+      {10 => ["Q", "Z"]}
+    ]
+
     def self.score(word)
-      SCORING.each do |score|
-        score.each do |score, letter|
-          if letter.include?
-          end
-        end
+      word.each do |letter|
+
+        
       end
     end
-
-    # def self.highest_score_from(array_of_words)
-    # end
-
   end
+
+
+  # def self.highest_score_from(array_of_words)
+  # end
+
+end
 end
 
 Scrabble::Scoring.score("potato")

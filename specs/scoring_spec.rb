55bad6b5d9c1a39bda7497b750ecaeaa49f83dd5
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-02-28 13:58:42 -0800

    organized test methods into describe-do blocks

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -8,10 +8,6 @@ Minitest::Reporters::SpecReporter.new
 
 describe "Scrabble::Scoring" do
 
-  # before do
-  #   @game = Scrabble::Scoring.new
-  # end
-
   # test if letters have correct values attached
   it "Letters have correct values" do
     Scrabble::Scoring::LETTER_VALUES[:A].must_equal 1
@@ -20,21 +16,41 @@ describe "Scrabble::Scoring" do
     Scrabble::Scoring::LETTER_VALUES[:Z].must_equal 10
   end
 
-# check that word is no more than 7 letters long
-it "Word is 7 letters or less" do
-  proc {
-  Scrabble::Scoring.score("supercalifragilistic")}.must_raise ArgumentError
+  describe "self.score" do
+    # check that word is no more than 7 letters long
+    it "Word is 7 letters or less" do
+      proc {
+      Scrabble::Scoring.score("supercalifragilistic")}.must_raise ArgumentError
+    end
+
+    # test that word is scored correctly and returns that score
+    it "Scores word correctly" do
+      Scrabble::Scoring.score("moo").must_equal 5
+    end
+
+    # also correct in case of: 7 letter word
+    it "Scores 7-letter word correctly" do
+      Scrabble::Scoring.score("hipster").must_equal 62
+    end
+  end
+
+  describe "highest_score_from" do
+    it "Scores all words correctly and returns highest scoring word" do
+    Scrabble::Scoring.highest_score_from(["moo", "hipster", "hippo", "muzjiks"]).must_equal "muzjiks"
+    end
+
+    # test that highest score handles ties
+    it "Handles ties correctly" do
+      Scrabble::Scoring.highest_score_from(["cat", "bat", "l", "a"]).must_equal "cat"
+    end
+
+    # tests that it handles 7 letter tie correctly
+    it "Handles 7 letter word ties correctly" do
+      Scrabble::Scoring.highest_score_from(["cat", "moooooo", "fb"]).must_equal "moooooo"
+    end
+  end
 end
 
-# test that word is scored correctly and returns that score
-it "Scores word correctly" do
-  Scrabble::Scoring.score("moo").must_equal 5
-end
-
-# also correct in case of: 7 letter word
-it "Scores 7-letter word correctly" do
-  Scrabble::Scoring.score("hipster").must_equal 62
-end
 
 # word and score are pushed to @wordarray
 # it "adds new word & score element to words array" do
@@ -47,17 +63,3 @@ end
 
 # test that highest score method calculates correctly
 # test that highest score is returned
-it "Scores all words correctly and returns highest scoring word" do
-Scrabble::Scoring.highest_score_from(["moo", "hipster", "hippo", "muzjiks"]).must_equal "muzjiks"
-end
-
-# test that highest score handles ties
-it "Handles ties correctly" do
-  Scrabble::Scoring.highest_score_from(["cat", "bat", "l", "a"]).must_equal "cat"
-end
-
-it "Handles 7 letter word ties correctly" do
-  Scrabble::Scoring.highest_score_from(["cat", "moooooo", "fb"]).must_equal "moooooo"
-end
-
-end

5c5a321e3c1032cdfede1ea914e3c9a68b9f666f
natalia-ku <bilberryfm@mail.ru>
2017-02-27 17:12:22 -0800

    added highest_score method

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -10,15 +10,39 @@ module Scrabble
       8 => %w(J X),
       10 => %w(Q Z)
     }
-    attr_reader :letters_values
 
     def initialize
 
     end
 
-  end
-end
+    def self.score(word)
+      word_array = word.upcase.split("")
+      total = 0
+      word_array.each do |letter|
+        LETTERS_VALUES.each do |key, value|
+          total += key if value.include? letter
+        end
+      end
+      if word.length >= 7
+        total += 50
+      end
+      return total
+    end
 
-score = Scrabble::Scoring.new
+    def self.highest_score_from(array_of_words)
+      max = 0
+      max_words = []
+      array_of_words.each do |word|
+        score_word = score(word)
+        if score_word == max
+          max_words.push word
+        elsif score_word > max
+          max = score_word
+          max_words = [word]
+        end
+      end
+      puts max_words
+    end
 
-puts Scrabble::Scoring::LETTERS_VALUES
+  end # end of class
+end # end of module
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -14,8 +14,33 @@ describe "Scoring class" do
     Scrabble::Scoring::LETTERS_VALUES.class.must_equal Hash
   end
 
+  describe "Scoring#score(word)" do
+    it "score returns an Integer" do
+      Scrabble::Scoring.score("Hello").class.must_equal Integer
+    end
+
+    it "Testing the right values" do
+      Scrabble::Scoring.score("H").must_equal 4
+      Scrabble::Scoring.score("Z").must_equal 10
+      Scrabble::Scoring.score("Hi").must_equal 5
+      Scrabble::Scoring.score("com").must_equal 7
+      Scrabble::Scoring.score("pu").must_equal 4
+      Scrabble::Scoring.score("ter").must_equal 3
+    end
+
+    it "50 extra points for 7 letters word" do
+      Scrabble::Scoring.score("jacuzzi").must_equal 84
+    end
+
+  end # end of score method
+
+  describe "Scoring#highest_score_from" do
+    it "" do
+    end
+
+  end
 
 
 
 
-end
+end # end of class

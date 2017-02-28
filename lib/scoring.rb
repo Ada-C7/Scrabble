df8f0ae5d05769960decf88f2faf65fa02b6cbf6
Ting Wong <hellotingwong@gmail.com>
2017-02-28 14:50:12 -0800

    basic self.score completed

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,28 +1,33 @@
 module Scrabble
+  attr_reader :word_score
   class Scoring
-    SCORE = [
-      {1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]},
-      {2 => ["D", "G"]},
-      {3 =>["B", "C", "M", "P"]},
-      {4 =>["F", "H", "V", "W", "Y"]},
-      {5 => ["K"]},
-      {8 => ["J", "X"]},
-      {10 => ["Q", "Z"]}
-    ]
+    SCORE = {
+      1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+      2 => ["D", "G"],
+      3 =>["B", "C", "M", "P"],
+      4 =>["F", "H", "V", "W", "Y"],
+      5 => ["K"],
+      8 => ["J", "X"],
+      10 => ["Q", "Z"]}
 
-    def self.score(word)
-      word.each do |letter|
-
-        
+      def self.score(word)
+        @word_score = 0
+        word.each_char do |letter|
+          @letter = letter.upcase
+          SCORE.each do |score, letter|
+            if letter.include? @letter
+              @word_score += score
+            end
+          end
+        end
+        return @word_score
       end
+
+
+      # def self.highest_score_from(array_of_words)
+      # end
+
     end
   end
 
-
-  # def self.highest_score_from(array_of_words)
-  # end
-
-end
-end
-
-Scrabble::Scoring.score("potato")
+  Scrabble::Scoring.score("potato")

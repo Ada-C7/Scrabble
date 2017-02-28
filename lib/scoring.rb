c0bae04faab3d0cb2d1a6015b1e2f8f4b336b298
add2point71dots <alcland@gmail.com>
2017-02-28 14:42:51 -0800

    Cleaned up score method

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -2,60 +2,23 @@ require 'csv'
 
 module Scrabble
   class Scoring
-     @@tiles = {}
+    @@tiles = {}
 
     CSV.read("./support/scoring.csv").each do |line|
-        @@tiles[line[0]] = line[1].to_i
-      end
-
-    # {
-    #   "A" => 1,
-    #   "E" => 1,
-    #   "I" => 1,
-    #   "O" => 1,
-    #   "U" => 1,
-    #   "L" => 1,
-    #   "N" => 1,
-    #   "R" => 1,
-    #   "S" => 1,
-    #   "T" => 1,
-    #   "D" => 2,
-    #   "G" => 2,
-    #   "B" => 3,
-    #   "C" => 3,
-    #   "M" => 3,
-    #   "P" => 3,
-    #   "F" => 4,
-    #   "H" => 4,
-    #   "V" => 4,
-    #   "W" => 4,
-    #   "Y" => 4,
-    #   "K" => 5,
-    #   "J" => 8,
-    #   "X" => 8,
-    #   "Q" => 10,
-    #   "Z" => 10
-    # }
-
+      @@tiles[line[0]] = line[1].to_i
+    end
 
     def self.score(word)
-      raise ArgumentError.new "That word is invalid" if word.length > 7 || word.length == 0
-
-      if (word =~ /^[a-zA-Z]+$/) != 0
-        raise ArgumentError.new "That's not a word."
-      end
+      raise ArgumentError.new "That words can't be longer than 7 letters." if word.length > 7
+      raise ArgumentError.new "Words must have at least one letter." if word.length == 0
+      raise ArgumentError.new "Words must only have letters." if (word =~ /^[a-zA-Z]+$/) != 0
 
       word.upcase!
-
       score = 0
 
-      word.each_char do |letter|
-        score += @@tiles[letter]
-      end
+      word.chars.each { |letter| score += @@tiles[letter] }
 
-      if word.length == 7
-        score += 50
-      end
+      score += 50 if word.length == 7
 
       return score
     end

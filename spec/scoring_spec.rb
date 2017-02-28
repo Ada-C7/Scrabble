13ada21c9be23c39551881fc5c1b1c53f845acc6
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-02-27 16:04:36 -0800

    implemented the first scoring tests - now need to write Scoring class code

--- spec/scoring_spec.rb
+++ spec/scoring_spec.rb
@@ -4,3 +4,30 @@ require 'minitest/skip_dsl'
 require_relative '../lib/scoring'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+describe Scoring do
+
+  score = Scrabble::Scoring.new
+  score.must_be_instance_of of Scrabble::Scoring
+
+  describe "Scoring#initialize" do
+
+    it "returns a letter_scores hash" do
+      score = Scrabble::Scoring.new
+      score.letter_scores.must_be_instance_of Hash
+    end
+
+    it "will throw error if given an arguments" do
+      proc {
+        Scrabble::Scoring.new("hello")
+      }.must_raise ArgumentError
+    end
+
+    it "returns a hash of length 26" do
+      score = Scrabble::Scoring.new.
+      score.length.must_equal 26
+    end
+
+  end
+
+end

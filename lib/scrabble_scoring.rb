67c10e510366351b826bb60a931e11b5014663b2
Hyunji Kim <ricecakemonster@gmail.com>
2017-02-28 14:05:50 -0800

    returns the total score of word

--- lib/scrabble_scoring.rb
+++ lib/scrabble_scoring.rb
@@ -13,16 +13,26 @@ module Scrabble
       return LETTERS
     end
     #
+    # @@letters = LETTERS
+
     def self.score(word)
-      word.length.times do |letter|
-        self.letter.each do |key|
-          self.letter.fetch(key).include?(letter)
+      total = []
+      word.each_char do |letter|
+        # print "Letter: #{letter}  \n"
+        LETTERS.each do |key, array|
+          # print "Key: #{key}  "
+          # print "Array: #{array}\n"
+          if array.include?(letter)
+            # print array
+            total << key
+          end
         end
       end
+      print total.inject(:+)
     end
 
   end
 end
 
-a = Scrabble::Scoring.new
-Scrabble::Scoring.score("yes")
+# # a = Scrabble::Scoring.new
+# print Scrabble::Scoring.score("BIRD")

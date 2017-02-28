1f8c67ed14fd7c6b8d3e12fbff8da399b15e9fd9
Ann Dai <gofarann@gmail.com>
2017-02-28 09:50:53 -0800

    corrected loop error in scorecard comparison. added summing of score array to return score

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,6 +1,9 @@
 module Scrabble
 
   class Scoring
+
+    attr_reader :SCOREBOARD
+
     SCOREBOARD = {
       %w[a e i o u l n r s t]=>1, %w[d g]=> 2, %w[b c m p]=>3, %w[f h v w y]=> 4, %w[k]=> 5, %w[j x]=> 8, %w[q z]=> 10
     }
@@ -8,21 +11,46 @@ module Scrabble
     def self.score word
       letter_values = []
 
-      word_array = word.scan /\w/
+      word_array = word.downcase.scan /\w/
 
-      word_array.each do |letter|
-        SCOREBOARD.each do |key, value|
+      SCOREBOARD.each do |key, value|
+        word_array.each do |letter|
           if key.include?(letter)
             letter_values << value
           else
             letter_values << 0
           end
-
         end
-        return word
       end
+
+
+      # word_array.each do |letter|
+      #   SCOREBOARD.each do |key, value|
+      #     if key.include?(letter)
+      #       letter_values << value
+      #     else
+      #       letter_values << 0
+      #     end
+      #   end
+
+      # this needs to go into the next black
+      points_count = 0
+      # puts letter_values
+      # puts word_array
+
+      letter_values.each do |points|
+        points_count += points
+      end
+
+      if word_array.size != 7
+        return points_count
+      else
+        return points_count + 50
+      end
+
     end
   end
+end
 
 
-  puts Scoring.score(word)
+puts Scrabble::Scoring.score("ooooooooo")

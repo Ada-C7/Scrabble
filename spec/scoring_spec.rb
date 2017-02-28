1c7dae121937ae62e0ac19f6722d1c1867b72972
Sahana Murthy <s.murthy730.com>
2017-02-27 16:48:45 -0800

    Created initial tests for score method

--- spec/scoring_spec.rb
+++ spec/scoring_spec.rb
@@ -7,9 +7,6 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "Scoring" do
 
-  # score = Scrabble::Scoring.new
-  # score.must_be_instance_of Scrabble::Scoring
-
   describe "Scoring#initialize" do
     it "initializes" do
       score = Scrabble::Scoring.new
@@ -34,4 +31,28 @@ describe "Scoring" do
 
   end
 
+  describe "Scoring#score" do
+    it "initializes" do
+      Scrabble::Scoring.score("hello").must_respond_to :score
+    end
+
+    it "Raises ArgumentError if no parameter given" do
+      proc {
+        Scrabble::Scoring.score
+      }.must_raise ArgumentError
+    end
+
+    # it "Receives a string input" do
+    #   word = "hello"
+    #   Scrabble::Scoring.score(word)
+    #   word.must_be_instance_of String
+    # end
+
+    it "Raises an ArgumentError if not string" do
+      word = 34355
+      proc {
+        Scrabble::Scoring.score(word)
+      }.must_raise ArgumentError
+    end
+    
 end

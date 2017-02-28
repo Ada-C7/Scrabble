63acb6f22937eb85a09a2cb0fc1ebd06bcf194b1
Allison Northrop <agnorthrop@gmail.com>
2017-02-27 17:01:31 -0800

    working on score method

--- lib/scrabble_scoring.rb
+++ lib/scrabble_scoring.rb
@@ -1,13 +1,28 @@
 module Scrabble
   class Scoring
-    LETTERS = [
-              {1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]},
-              {2 => ["D","G"]},
-              {3 => ["B", "C", "M", "P"]},
-              {4 => ["F","H","V","W","Y"]},
-              {5 => ["K"]},
-              {8 => ["J", "X"]},
-              {10 => ["Q", "Z"]}
-            ]
+    LETTERS = {
+              1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+              2 => ["D","G"],
+              3 => ["B", "C", "M", "P"],
+              4 => ["F","H","V","W","Y"],
+              5 => ["K"],
+              8 => ["J", "X"],
+              10 => ["Q", "Z"]
+            }
+    def letters
+      return LETTERS
+    end
+    #
+    def self.score(word)
+      word.length.times do |letter|
+        self.letter.each do |key|
+          self.letter.fetch(key).include?(letter)
+        end
+      end
+    end
+
   end
 end
+
+a = Scrabble::Scoring.new
+Scrabble::Scoring.score("yes")
--- specs/scrabble_scoring_spec.rb
+++ specs/scrabble_scoring_spec.rb
@@ -7,7 +7,19 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 describe "Scrabble Scoring" do
 it "creates a scrabble scoring class" do
   game_1 = Scrabble::Scoring.new
-  game_1.must_be_kind_of Scrabble::Scoring 
+  game_1.must_be_kind_of Scrabble::Scoring
 end
+
+it "the class contains a data structure" do
+  game_1 = Scrabble::Scoring.new
+  game_1.letters.must_be_kind_of Array
+end
+
+it "the method self.score(word) should return total score" do
+  game_1 = Scrabble::Scoring.new
+  game_1.score("yay").must_equal 9
+end
+
+
 
 end

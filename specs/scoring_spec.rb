038e4b10f1d51064e0867f1a67a9308346f97e04
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-02-27 16:45:01 -0800

    organized tests

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -6,21 +6,22 @@ require_relative '../lib/scoring'
 
 describe "Module Scrabble, Scoring Class" do
   it "exists" do
-
     skip
-
-  end
-
-  it "self.highest_word returns string"do
-  self.highest_word.must_be_kind_of String
-
-    # skip
     Scrabble::Scoring.class.must_be_kind_of Class
   end
+end
+
+describe "self.score" do
   it "self.score returns an integer" do
-    # skip
+    skip
     Scrabble::Scoring.score("apple").must_be_kind_of Integer
     Scrabble::Scoring.score("apple").must_equal 9
+  end
+end
 
+describe "self.highest_word" do
+  it "self.highest_word returns string" do
+    skip
+    self.highest_word.must_be_kind_of String
   end
 end

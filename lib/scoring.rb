443682c94c5673492bb102ccfb8915d42c920f22
Danielle Birbal <birbalds@gmail.com>
2017-02-27 16:11:07 -0800

    Merging Files

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,30 +1,6 @@
 module Scrabble
-  attr_reader :score
-  class Scoring
-    SCORE_CHART = {
-      1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-      2 => ["D", "G"],
-      3 => ["B", "C", "M", "P"],
-      4 => ["F", "H", "V", "W", "Y"],
-      5 => ["K"],
-      8 => ["J", "X"],
-      10 => ["Q", "Z"]
-    }
-    def initialize
-    end
+    class Scoring
 
-    def self.score(word)
-      word.each_char do |letter|
-        SCORE_CHART.each do |number, alpha|
-          if alpha.includes?(letter)
-            letter_score = number
-          end
-          score += letter_score
-        end
-        return score
-      end
+      def self.highest_score_from(array_of_words)
     end
-  end
 end
-
-print Scrabble.score("rabbit")

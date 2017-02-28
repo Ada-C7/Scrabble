f789f24c9b42c722492e9eebb07cc575d0cfe4ec
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-02-28 14:45:53 -0800

    wave 1 complete - both methods implemented and all tests passing

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,20 +1,10 @@
 module Scrabble
   class Scoring
-    # LETTER_SCORES = { a: 1, c: 3, b: 3, e: 1, d: 2, g: 2, f: 4, i: 1, h: 4, k: 5, j: 8,
-    #                 m: 3, l: 1, o: 1, n: 1, q: 10, p: 3, s: 1, r: 1, u: 1, t: 1, w: 4,
-    #                 v: 4, y: 4, x: 8, z: 10 }
     LETTER_SCORES = {"a" => 1, "c" => 3, "b" => 3, "e" => 1, "d" => 2, "g" => 2,
              "f" => 4, "i" => 1, "h" => 4, "k" => 5, "j" => 8, "m" => 3,
              "l" => 1, "o" => 1, "n" => 1, "q" => 10, "p" => 3, "s" => 1,
              "r" => 1, "u" => 1, "t" => 1, "w" => 4, "v" => 4, "y" => 4,
              "x" => 8, "z" => 10}
-
-    # attr_reader :letter_scores
-    #
-    # # refractor this to a constant - go back to strings?
-    # def initialize
-    #
-    # end
 
     def self.tie_breaker(tie_words)
       tie_words.each do |word|
@@ -24,33 +14,21 @@ module Scrabble
           return tie_words.min_by(&:length)
         end
       end
-      #  unless tie_words.select { |word| word.length == 7 }
     end
 
     def self.score(word)
       unless word.class == String
         raise ArgumentError.new "Input word must be a string"
       end
-      # Not sure how the asterisk works -- inspired from Stack Overflow
       word_score = LETTER_SCORES.values_at(*word.chars).reduce(:+)
       word.length == 7 ? word_score += 50 : word_score
     end
 
     def self.highest_score_from(words)
       raise ArgumentError if words.class != Array
-      # will return a hash where key is the score and value is array of the word(s)
-      # with that score
       scores_and_words = words.group_by { |word| score(word) }
-      # now we need to get the word we want out of the scores hash
-      # we want to return the value of the higest key
-      # this returns an array with the score at idex zero and word list at index 1
       highest_words = scores_and_words.max_by { |score, word_list| score }
       highest_words[1].length > 1 ? tie_breaker(highest_words[1]) : highest_words[1].join
     end
   end
 end
-
-# print Scrabble::Scoring.highest_score_from(["ox", "meow", "cat", "ax"])
-# puts
-# print Scrabble::Scoring.highest_score_from( ["algebra", "qzqzqz"])
-# puts
--- spec/scoring_spec.rb
+++ spec/scoring_spec.rb
@@ -7,30 +7,6 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "Scoring" do
 
-  # describe "Scoring#initialize" do
-  #   it "initializes" do
-  #     score = Scrabble::Scoring.new
-  #     score.must_be_instance_of Scrabble::Scoring
-  #   end
-  #
-  #   it "returns a letter_scores hash" do
-  #     score = Scrabble::Scoring.new
-  #     score.letter_scores.must_be_kind_of Hash
-  #   end
-  #
-  #   it "will throw error if given an arguments" do
-  #     proc {
-  #       Scrabble::Scoring.new("hello")
-  #     }.must_raise ArgumentError
-  #   end
-  #
-  #   it "returns a hash of length 26" do
-  #     score = Scrabble::Scoring.new
-  #     score.letter_scores.length.must_equal 26
-  #   end
-  #
-  # end
-
   describe "Scoring#score" do
 
     it "initializes" do
@@ -42,12 +18,6 @@ describe "Scoring" do
         Scrabble::Scoring.score
       }.must_raise ArgumentError
     end
-
-    # it "Receives a string input" do
-    #   word = "hello"
-    #   Scrabble::Scoring.score(word)
-    #   word.must_be_instance_of String
-    # end
 
     it "Raises an ArgumentError if not string" do
       word = 34355

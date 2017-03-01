7abd973f801e59c49ebffeab8dd8bf976369fd2d
Jackie Watanabe <sushiigirl@gmail.com>
2017-03-01 10:59:13 -0800

    created test to check for array when plays is called. also added plays attribute

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -11,5 +11,11 @@ describe "Wave 2" do
     it "Takes a name" do
       @player.must_respond_to :name
     end
+
+    it "returns an Array of the words played by the player" do
+      @player.must_respond_to :plays
+      @player.plays.must_be_instance_of Array
+    end
+
   end
 end

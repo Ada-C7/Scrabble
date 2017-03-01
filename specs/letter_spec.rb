8ed0abfb7f52b19e121eccf57a54ccb2b927ef5a
janicewilson <janice.wilson@mac.com>
2017-03-01 11:23:14 -0800

    Tests added.

---
+++ specs/letter_spec.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require_relative '../lib/letter'
+
+Minitest::Reporters.use!
+Minitest::Reporters::SpecReporter.new
+
+describe Scrabble::Letter do
+
+     before do
+
+          @my_letter = Scrabble::Letter.new("l")
+     end
+
+
+     it "Takes a letter" do
+
+          @my_letter.must_respond_to :name
+          @my_letter.name.must_equal "L"
+
+     end
+
+     it "Is a kind of Letter" do
+
+          @my_letter.must_be_kind_of Scrabble::Letter
+
+     end
+
+     it "Assigns the correct points to letter" do
+
+          @my_letter.point.must_equal 1
+
+     end
+
+
+end
---
+++ specs/score_spec.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require_relative '../lib/score'
+require_relative '../lib/letter'
+
+
+Minitest::Reporters.use!
+Minitest::Reporters::SpecReporter.new
+
+
+describe Scrabble::Score do
+
+     array = %w(Returns a hash where the keys are the evaluated result from the block and the values are arrays of elements in the collection that correspond to the key)
+
+     describe self.score(word) do
+
+
+
+     end
+
+     describe self.highest_score_from(words) do
+
+     end
+
+end

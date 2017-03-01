cf102493a72ae6559a6bf1fab78138f0f7062e28
Hyunji Kim <ricecakemonster@gmail.com>
2017-03-01 12:28:11 -0800

    Finished writing test file

--- specs/scrabble_player_spec.rb
+++ specs/scrabble_player_spec.rb
@@ -28,3 +28,62 @@ describe "Player#initialize" do
     player.plays.must_be_empty
   end
 end
+
+describe "Player#play" do
+  it "Raises an ArgumentError when other than alphabets are entered" do
+    player = Scrabble::Player.new("Mickey")
+    proc {
+      player.play(23)
+    }.must_raise ArgumentError
+  end
+
+  it "Returns false if player has already won" do
+    player = Scrabble::Player.new("Mickey")
+    player.won?
+    player.play("whatever").must_equal false
+  end
+
+  it "Returns the score of the word" do
+    player = Scrabble::Player.new("Mickey")
+    player.play("red").must_equal 4
+  end
+end
+
+describe "Player#total_score" do
+  it "Returns the sum of scores of played words"do
+    player = Scrabble::Player.new("Mickey")
+    player.play("red")
+    player.play("white")
+    player.play("black")
+    player.total_score.must_equal 28
+  end
+end
+
+describe "Player#won?" do
+  it "Returns true, if the player has over 100 points" do
+    player = Scrabble::Player.new("Mickey")
+    player.play("qqqqq")
+    player.play("zzzzz")
+    player.won.must_equal true
+  end
+end
+
+describe "Player#highest_scoring_word" do
+  it "Returns the highest scoring played word" do
+    player = Scrabble::Player.new("Mickey")
+    player.play("red")
+    player.play("white")
+    player.play("black")
+    player.highest_scoring_word.must_equal "black"
+  end
+end
+
+describe "Player#higest_word_score" do
+  it "Returns the highest_scoring_word score" do
+    player = Scrabble::Player.new("Mickey")
+    player.play("red")
+    player.play("white")
+    player.play("black")
+    player.highest_word_score.must_equal 13
+  end
+end

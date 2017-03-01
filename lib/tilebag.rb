d5f21b287c750ee58fd5d989860c853c2580a944
Alix Hamilton <alix.hamilton@gmail.com>
2017-03-01 12:42:46 -0800

    refactors tilebag

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -1,33 +1,28 @@
 module Scrabble
   class TileBag
 
+    STARTING_TILE_COUNT = {
+      "A"=>9, "B"=>2, "C"=>2, "D"=>4,
+      "E"=>12, "F"=>2, "G"=>3, "H"=>2,
+      "I"=>9, "J"=>1, "K"=>1, "L"=>4,
+      "M"=>2, "N"=>6, "O"=>8, "P"=>2,
+      "Q"=>1, "R"=>6, "S"=>4, "T"=>6,
+      "U"=>4, "V"=>2, "W"=>2, "X"=>1,
+      "Y"=>2, "Z"=>1
+    }
+
     def initialize
-      @default_tiles = {
-        "A"=>9, "B"=>2, "C"=>2, "D"=>4,
-        "E"=>12, "F"=>2, "G"=>3, "H"=>2,
-        "I"=>9, "J"=>1, "K"=>1, "L"=>4,
-        "M"=>2, "N"=>6, "O"=>8, "P"=>2,
-        "Q"=>1, "R"=>6, "S"=>4, "T"=>6,
-        "U"=>4, "V"=>2, "W"=>2, "X"=>1,
-        "Y"=>2, "Z"=>1
-      }
-      @available_letters = ("A".."Z").to_a
+      @available_letters = STARTING_TILE_COUNT.map {
+        |key, value| (key * value).chars
+      }.flatten.shuffle
     end
 
     def draw_tiles(num)
-      tiles = []
-
-      num.times do
-        letter = @available_letters.sample
-        tiles << letter
-        @default_tiles[letter] -= 1
-        @available_letters.delete(letter) if @default_tiles[letter] == 0
-      end
-      return tiles
+      @available_letters.pop(num)
     end
 
     def tiles_remaining
-      @default_tiles.values.reduce(:+)
+      @tile_counts.values.reduce(:+)
     end
   end
 end

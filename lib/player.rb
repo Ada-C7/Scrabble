9233280b616d477166da3090badd4ee0846ab076
Jackie Watanabe <sushiigirl@gmail.com>
2017-03-01 13:31:08 -0800

    made highest_word_score its own method again

--- lib/player.rb
+++ lib/player.rb
@@ -2,13 +2,14 @@ require_relative 'scoring'
 
 module Scrabble
   class Player
-    attr_reader :name, :plays, :total_score, :highest_word_score
+    attr_reader :name, :plays, :total_score
+    # :highest_word_score
 
     def initialize(name)
       @name = name
       @plays =[]
       @total_score = 0
-      @highest_word_score
+      # @highest_word_score = 0
     end
 
     def play(word)
@@ -17,27 +18,28 @@ module Scrabble
       @plays << word
       @total_score += score
 
-      highest_scoring_word
+      # highest_scoring_word
       return score
     end
 
     def highest_scoring_word
       word = Scoring.highest_score_from(@plays)
-      @highest_word_score = Scoring.score(word)
+      # @highest_word_score = Scoring.score(word)
       return word
     end
 
-    # def highest_word_score
-    #   return Scoring.score(highest_scoring_word)
-    # end
+    def highest_word_score
+      return Scoring.score(highest_scoring_word)
+    end
 
     def won?
-      if @total_score > 100
-        true
-      else
-        false
-      end
+      return @total_score > 100
     end
 
   end
 end
+
+# player = Scrabble::Player.new("Mary")
+#
+# # player.highest_scoring_word
+# puts "#{player.highest_word_score}"

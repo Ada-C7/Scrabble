f156544f905f7e89ccb98f8a04db50e29c53b344
sai samant <saisamant@gmail.com>
2017-03-01 13:45:17 -0800

    code for player methods

--- lib/player.rb
+++ lib/player.rb
@@ -5,31 +5,37 @@ module Scrabble
 
     def initialize name
       @name = name
-    end
-
-    def plays
-      #plays: returns an Array of the words played by the player
+      @plays = []
     end
 
     def play word
+      return false if player.won?
+
+      @plays << word
+      return Scoring.score(word)
       #play(word): Adds the input word to the plays Array
       # Returns false if player has already won
       # Returns the score of the word
     end
 
-    def total_score
+    def total_score @plays
+      scores = @plays.map { |word| Scoring.score(word) }
+      return scores.sum
       # Returns the sum of scores of player words
     end
 
     def won?
+      total_score > 100 ? true : false
       # If the player has over 100 points, returns true, otherwise returns false
     end
 
-    def highest_scoring_word 
+    def highest_scoring_word
+      return Scoring.highest_score_from(@plays)
       # Returns the highest scoring played word
     end
 
     def highest_word_score
+      Scoring.score(highest_scoring_word)
       # Returns the highest_scoring_word score
     end
 

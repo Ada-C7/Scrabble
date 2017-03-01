2ea0b234175bf25d4fde877ebc339674e5842e11
Sahana Murthy <s.murthy730.com>
2017-03-01 11:09:56 -0800

    Includes tests for Player class initialize method. Also includes first draft of Player class initialize program.

---
+++ spec/player_spec.rb
@@ -0,0 +1,55 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require_relative '../lib/scoring.rb'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+describe "Player" do
+
+  describe "Player#initialize" do
+    before do
+      @name = "Ada"
+      @player = Scrabble::Player.new(@name)
+    end
+
+    it "initializes" do
+      @player.must_be_instance_of Scrabble::Player
+    end
+
+    it "Takes a name" do
+      @player.must_respond_to :name
+      @player.name.must_equal @name
+    end
+
+    it "Raises ArgumentError if no name is given" do
+      proc {
+        Scrabble::Player.new
+      }.must_raise ArgumentError
+    end
+
+    it "Raises an ArgumentError if not string" do
+      proc {
+        Scrabble::Player.new(bfhdsfbd)
+      }.must_raise ArgumentError
+    end
+
+    it "Contains an array of the words played by the player" do
+      plays = []
+      @player.must_respond_to :plays
+      @player.plays.must_be_kind_of Array
+      @player.plays.must_equal plays
+    end
+
+    it "Contains an array of strings" do
+      plays = [ldkf, s;dlfk, jfg]
+      proc {
+        @player.plays
+      }.must_raise ArgumentError
+    end 
+
+  end
+
+end

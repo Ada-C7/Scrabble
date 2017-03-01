6bb60d40e1c88cbb47437830ea8cec63a9776c87
Kayla <kubicke.kayla@gmail.com>
2017-02-28 17:13:31 -0800

    Finished self.score and test.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,51 +1,52 @@
 module Scrabble
   class Scoring
 
-    SCORE = 12
+    @letter_hash = {
+      "A"=>1,
+      "B"=>3,
+      "C"=>3,
+      "D"=>2,
+      "E"=>1,
+      "F"=>4,
+      "G"=>2,
+      "H"=>4,
+      "I"=>1,
+      "J"=>8,
+      "K"=>5,
+      "L"=>1,
+      "M"=>3,
+      "N"=>1,
+      "O"=>1,
+      "P"=>3,
+      "Q"=>10,
+      "R"=>1,
+      "S"=>1,
+      "T"=>1,
+      "U"=>1,
+      "V"=>4,
+      "W"=>4,
+      "X"=>8,
+      "Y"=>4,
+      "Z"=>10
+    }
 
-    def initialize
-    end
-
-    def variables
-      @letter_hash = {
-        "A"=>1,
-        "B"=>3,
-        "C"=>3,
-        "D"=>2,
-        "E"=>1,
-        "F"=>4,
-        "G"=>2,
-        "H"=>4,
-        "I"=>1,
-        "J"=>8,
-        "K"=>5,
-        "L"=>1,
-        "M"=>3,
-        "N"=>1,
-        "O"=>1,
-        "P"=>3,
-        "Q"=>10,
-        "R"=>1,
-        "S"=>1,
-        "T"=>1,
-        "U"=>1,
-        "V"=>4,
-        "W"=>4,
-        "X"=>8,
-        "Y"=>4,
-        "Z"=>10
-      }
-    end
 
     def self.score(word)
+      score = 0
       word_array = []
-      word.split('').map { |s| word_array << s }
-      return word_array
-    end
+      word = word.upcase
+      word.split('').map { |s| word_array << s } #raise argument error
 
-    def self.highest_score_from(array_of_words)
+      word_array.each do |letter|
+        score += @letter_hash[letter]
+      end
+      return score
     end
+  end
+
+  def self.highest_score_from(array_of_words)
   end
 end
 
-print Scrabble::Scoring.score("apple")
+
+puts Scrabble::Scoring.score("apple")
--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -9,41 +9,48 @@ describe "Wave 1" do
 
   describe "Score method" do
     it "total score for given word" do
-      score.must_output 12
+      @test_1 = Scrabble::Scoring.score("apple")
+      @test_1.must_equal 9
     end
+
     it "word input must be string" do
-
+      # skip
+      word = "apple"
+      @test_1 = Scrabble::Scoring.score(word)
+      @test_1.class.must_equal String #Proc test, raise arugment error
     end
+
     it "make sure word is case sensitive" do
-
+      skip
     end
-    it "word should return correct score" do
 
+    it "word should return correct score" do
+      skip
     end
   end
 
   describe "highest_score_from method" do
     it "test for empty array" do
-
+      skip
     end
     it "return highest scoring word from array" do
-
+      skip
     end
   end
 
   describe "tiebreaking rules" do
     it "recognize two words with same score" do
-
+      skip
     end
     it "if multiple words prefer word with less letters" do
-
+      skip
     end
     it "if multiple words prefer word that uses all seven letters" do
-
+      skip
     end
 
     it "if multiple words prefer word that was supplied first" do
-
+      skip
     end
 
   end

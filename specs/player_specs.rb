a907df861432e74cd4820aa66d0e9236bb901e3c
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-03-01 11:47:28 -0800

    created new specs file

---
+++ specs/player_specs.rb
@@ -0,0 +1,18 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+# require 'minitest/skip_dsl'
+require 'minitest/pride'
+require_relative '../lib/player'
+
+describe "Player Class" do
+
+
+
+
+
+
+  
+end

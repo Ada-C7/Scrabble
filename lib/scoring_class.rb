894133eb87265eea7c487d8fb493b5253953f7f5
Olivia <ssecretsharer@gmail.com>
2017-02-28 16:43:08 -0800

    First commit of scoring_class. In theory, first self.score method works, compares against score_chart

--- lib/scoring_class.rb
+++ lib/scoring_class.rb
@@ -2,31 +2,53 @@ module Scrabble
 
   class Scoring
 
-    SCORE_CHART = [{}]
+    SCORE_CHART =
+    {1 => ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t" ],
+      2 => ["d", "g" ],
+      3 => ["b", "c", "m", "p" ],
+      4 => ["f", "h", "v", "w", "y"],
+      5 => ["k"],
+      8 => ["j", "x"],
+      10 => ["q","z"] }
 
-    def self.score(word)
-      #grab word from user, into array_of_words
-      #split word into characters array
-      #make new array of characters
-      #convert chars to INT using each_index?
-      #sum all numbers from letters in word
-      #
-      #stores in array of scores
-    end
+      def initialize
+      end
 
-    def score_keeper
-      #grab score from scores array
-      #create an index of words played
+      def self.score(word)
+        @array_of_words = []
+        @word_score = 0
+        letter_points = []
+        array_of_words << word
+        split_word = word.chars
+
+        #puts split_word #test
+        split_word.each do |letter|
+          SCORE_CHART.each_pair do |num_key, array_of_letters|
+            if array_of_letters.include? letter
+              letter_points << key
+            end
+          end
+
+          letter_points.each do |point|
+            return @word_score += point
+          end
+        end #end of meth
+
+        def score_keeper
+          @array_of_words << word
+
+          #hash of words:scores
+          @array_of_words.zip.to_h(:word, :score)
+        end
+      end
 
 
-    end
+      def self.highest_score_from(array_of_words)
+        #.length or max_by to find
+        #longest word in array_of_words
+        #return longest word
 
+      end
 
-    def self.highest_score_from(array_of_words)
-      #.length or max_by to find
-      #longest word in array_of_words
-      #return longest word
-
-    end
-  end
-end
+    end #end of class
+  end #end of mod

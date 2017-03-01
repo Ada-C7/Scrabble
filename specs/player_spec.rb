660cbd89af9626f226db08a72658a0cd33c909c9
tehut <tehut.m.g@gmail.com>
2017-03-01 12:22:06 -0800

    wrote test for .name method

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -8,12 +8,23 @@ require 'minitest/pride'
 require_relative '../lib/player'
 
 describe "Player Class" do
+before do  "Create an instance"
+  tofu = Scrabble::Player.new
 
   describe "Module Scrabble, Player Class" do
     it "exists" do
       # skip
-      Scrabble::Player.class.must_be_kind_of Class
+      tofu.class.must_be_kind_of Class
     end
+
+  describe "Player class has a name" do
+    it "Will return a string when .name is called"
+    tofu.name.must_be_kind_of String
+    end
+  end
+
+
+
   end
 
 

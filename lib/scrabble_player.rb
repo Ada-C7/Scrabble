658a7386e03c15a26d4c2f120113e067c2f29bb6
Lynn Trickey <lynn.trickey@gmail.com>
2017-03-01 15:57:01 -0800

    Pulling out Total Score to be it's own Method.  Updated some tests.

--- lib/scrabble_player.rb
+++ lib/scrabble_player.rb
@@ -1,24 +1,17 @@
 module Scrabble
 
   class Player
-    attr_reader :name, :plays, :total_score
+    attr_reader :name, :plays
 
     def initialize(name)
       raise ArgumentError.new("Name input must be a String") if name.class != String
       @name = name
       @plays = []
-      @total_score = 0
     end
-
-    #plays return array of words played by player
-    # def plays
-    #   return @plays
-    # end
 
     #return true of player has > 100 points
     def won?
-      return @total_score > 100 ? true : false
-      #maybe score needs to be instance variable?
+      return total_score > 100 ? true : false
     end
 
     def play(word)
@@ -29,35 +22,27 @@ module Scrabble
         @plays << word
       end
       word_score = Scrabble::Scoring.score(word)
-      @total_score += word_score
       return word_score
     end
 
-
-    # def total_score
-    #   score = 0
-    #   @plays.each do |word|
-    #     score += Scrabble::Scoring.score(word)
-    #   end
-    #   score
-    # end
-
+    def total_score
+      score = 0
+      @plays.each do |word|
+        score += Scrabble::Scoring.score(word)
+      end
+      score
+    end
 
     #returns the highest scoring played word
     def highest_scoring_word
-      #access @plays
       Scrabble::Scoring.highest_score_from(@plays)
     end
 
     #return the highest scoring word's
     #word score
     def highest_word_score
-      #run highest_scoring_word METHOD
       highest_word = Scrabble::Scoring.highest_score_from(@plays)
       return Scrabble::Scoring.score(highest_word)
-
-      #run that return through Scrabble:Scoring.score(word)
-      #return as local variable.
     end
   end
 end
--- specs/scrabble_player_spec.rb
+++ specs/scrabble_player_spec.rb
@@ -15,59 +15,75 @@ describe "Player Class" do
       proc {
         Scrabble::Player.new(123)}.must_raise ArgumentError
       end
+    end
 
-      describe "Play" do
-        it "Word must be a string" do
-          proc { @new_player.play(123)}.must_raise ArgumentError
-        end
+  describe "Play" do
+    it "Word must be a string" do
+      proc { @new_player.play(123) }.must_raise ArgumentError
+    end
 
-        it "object.play(word) would return correct score"  do
-          @new_player.play("hi").must_equal 5
-        end
+    it "object.play(word) would return correct score as a fixnum"  do
+      score = @new_player.play("hi")
+      score.must_equal 5
+      score.must_be_instance_of Fixnum
+    end
 
-        it "saves word to @plays array" do
-          @new_player.play("hi")
-          @new_player.plays.must_equal ["hi"]
-        end
+    it "saves word to @plays array" do
+      @new_player.play("hi")
+      @new_player.plays.must_equal ["hi"]
+    end
 
-        it "returns fail if player has already won" do
-          @new_player.play("zzzzzzz")
-          @new_player.play("hi").must_equal false
-        end
+    it "returns fail if player has already won" do
+      @new_player.play("zzzzzzz")
+      @new_player.play("hi").must_equal false
+    end
 
+  end
+
+  describe "#total_score" do
+    it "returns the sum score of all the words played." do
+      3.times { @new_player.play("hi") }
+      @new_player.total_score.must_equal 15
       end
 
-      describe "#total_score" do
-        it "returns the sum score of all the words played." do
-          3.times { @new_player.play("hi") }
-          @new_player.total_score.must_equal 15
-        end
+    it "should output 0 if player has not played any words" do
+      @new_player.total_score.must_equal 0
+    end
+  end
+
+  describe "#won?" do
+    it "should return a value of true if score > 100" do
+        @new_player.play("zzzzzzz")
+        @new_player.won?.must_equal true
+        @new_player.won?.must_be_instance_of TrueClass
       end
 
-      describe "#won?" do
-        it "should return a value of true if score > 100" do
-          @new_player.play("zzzzzzz")
-          @new_player.won?.must_equal true
-        end
-
-        describe "#highest_scoring_word" do
-          it "returns the highest scoring played word" do
-            @new_player.play("hi")
-            @new_player.play("day")
-            @new_player.highest_scoring_word.must_equal "day"
-          end
-        end
-        describe "#highest_word_score" do
-          it "returns the score of the winning word" do
-            @new_player.play("hi")
-            @new_player.play("day")
-            @new_player.highest_scoring_word.must_equal "day"
-            @new_player.highest_word_score.must_equal 7
-          end
-        end
+    it "should return false if score is < 100" do
+        @new_player.won?.must_equal false
+        @new_player.won?.must_be_instance_of FalseClass
       end
 
     end
 
+    describe "#highest_scoring_word" do
+      it "returns the highest scoring played word" do
+        @new_player.play("hi")
+        @new_player.play("day")
+        @new_player.highest_scoring_word.must_equal "day"
+        end
+
+      it "returns the shortest word if a tie in length" do
+
+      end  
+      end
+
+    describe "#highest_word_score" do
+      it "returns the score of the winning word" do
+        @new_player.play("hi")
+        @new_player.play("day")
+        @new_player.highest_scoring_word.must_equal "day"
+        @new_player.highest_word_score.must_equal 7
+      end
+    end
 
   end

0d3b39b203b32254f846ca2f259699f068014674
Spatterjaaay <aja.pursova@gmail.com>
2017-03-01 15:51:19 -0800

    resolving merge

--- lib/score.rb
+++ lib/score.rb
@@ -34,12 +34,12 @@ module Scrabble
 # puts sample.class
 # puts sample
 # puts sample.inspect
-puts sample.score("cat")
-
-array = %w(A letter means that pliyer pleyer a used all the tiles)
-
-
-puts sample = Scrabble::Scoring.highest_score_from(array)
+# puts sample.score("cat")
+#
+# array = %w(A letter means that pliyer pleyer a used all the tiles)
+#
+#
+# puts sample = Scrabble::Scoring.highest_score_from(array)
 
 end
 

cf60bbd5ae7c250abaf795bf143232d47c5b4dfa
Kelly Souza <kellysouza99@gmail.com>
2017-03-01 12:52:36 -0800

    play(word) returns false or word score

--- lib/player.rb
+++ lib/player.rb
@@ -4,14 +4,18 @@ module Scrabble
 
 class Player
   attr_reader :name
-  attr_accessor :play
+  attr_accessor :play, :score
 
   def initialize(name)
     @name = name
     @play = []
+    @score = 0
   end
 
   def play(word)
+    return false if score >= 100
+    @play << word
+    return Scoring.score(word)
   end
 
   def total_score
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -21,6 +21,19 @@ describe "Player class" do
 end
 
 describe "Play method" do
+
+  before do
+    @player = Scrabble::Player.new('Ada')
+  end
+
+  it "returns false if score > 100" do
+    @player.score = 130
+    @player.play("cat").must_equal false
+end
+
+it "returns word score" do
+  @player.score = 10
+  @player.play("cat").must_equal 5
 end
 
 describe "Total Score" do
@@ -30,4 +43,5 @@ describe "Won" do
 end
 
 describe "Highest Scoring Word and Word Score" do
+end
 end

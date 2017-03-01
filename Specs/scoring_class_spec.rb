04ff97601efd325b9f8598934595e52e19be9c99
ashtn <ahstonehorton@gmail.com>
2017-02-28 22:13:41 -0800

    added edge case stub for two words

--- Specs/scoring_class_spec.rb
+++ Specs/scoring_class_spec.rb
@@ -12,16 +12,20 @@ describe "Score Method for Scoring Class" do
     @edge_word = "W@ter"
   end
 
-it "Raises argumentError if word contains anything other than letters" do
-  skip
-  proc { Scrabble::Scoring::score(@edge_word) }.must_raise ArgumentError
-end
+  it "Raises argumentError if word contains anything other than letters" do
+    skip
+    proc { Scrabble::Scoring::score(@edge_word) }.must_raise ArgumentError
+  end
+
+  it "Handles if the word passed has a space (two words)" do
+    skip #unknown outcome
+  end
 
 
   it "Score Method outputs integer" do
-  puts Scrabble::Scoring::score(@word).must_be_instance_of Integer
+    puts Scrabble::Scoring::score(@word).must_be_instance_of Integer
   end
-
+  
   it "Score method will total the value of each letter in the word" do
 
     Scrabble::Scoring::score(@word).must_equal 10

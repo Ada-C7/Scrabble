8ad41cbaf7befa05c2addf1a48f5515db42cbaeb
tehut <tehut.m.g@gmail.com>
2017-03-01 14:44:25 -0800

    tested tests after updating player.rb

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -11,51 +11,51 @@ describe "Player Class" do
   describe "tests tehut wrote" do
     describe "Test that Player Class exists" do
       it "exists" do
-        skip
-        tofu = Scrabble::Player.new
+        # skip
+        tofu = Scrabble::Player.new("tofu")
         tofu.class.must_be_kind_of Class
       end
     end
 
     describe "Player class has a name" do
       it "Will return a string when .name is called" do
-        skip
-        tofu = Scrabble::Player.new
+        # skip
+        tofu = Scrabble::Player.new("tofu")
         tofu.name.must_be_kind_of String
       end
     end
 
       describe ".plays initializes as an array" do
         it "is an array upon instantiation" do
-          skip
-          tofu = Scrabble::Player.new
+          # skip
+          tofu = Scrabble::Player.new("tofu")
           tofu.plays.must_be_kind_of Array
         end
       end
 
 
     describe "Test the .play Method" do
-      describe "Test play method returns an array" do
+      describe "Test play method returns an integer" do
           it "will return an array when called" do
-            skip
-            tofu = Scrabble::Player.new
-            tofu.play("snow").must_be_kind_of Array
+            # skip
+            tofu = Scrabble::Player.new("tofu")
+            tofu.play("snow").must_be_kind_of Integer
           end
         end
 
       describe ".play(word) returns the score" do
         it "returns the score if won == false" do
-          skip
-          tofu = Scrabble::Player.new
-          tofu.play("snow").must_equal 7 if tofu.win? == false
+          # skip
+          tofu = Scrabble::Player.new("tofu")
+          tofu.play("snow").must_equal 7 if tofu.won? == false
         end
       end
 
       describe ".play(word) returns false if player has won" do
-        it "returns false if .win? == true" do
-          skip
-            tofu = Scrabble::Player.new
-            tofu.play("snow").must_equal false if tofu.win? == true
+        it "returns false if .won? == true" do
+          # skip
+            tofu = Scrabble::Player.new("tofu")
+            tofu.play("snow").must_equal false if tofu.won? == true
         end
       end
 
@@ -63,8 +63,8 @@ describe "Player Class" do
 
   describe "total_score method" do
     it "returns the correct score" do
-      skip
-      ginny = Scrabble::Player.new
+      # skip
+      ginny = Scrabble::Player.new("ginny")
       ginny.play("apple")
       ginny.play("apple")
       ginny.total_score.must_equal 18
@@ -73,32 +73,32 @@ describe "Player Class" do
 
   describe "highest_scoring_word" do
     it "returns a string" do
-      skip
-      ginny = Scrabble::Player.new
+      # skip
+      ginny = Scrabble::Player.new("ginny")
       ginny.play("apple")
       ginny.play("peaches")
       ginny.highest_scoring_word.must_be_kind_of String
     end
     it "returns the correct value" do
-      skip
-      ginny = Scrabble::Player.new
+      # skip
+      ginny = Scrabble::Player.new("ginny")
       ginny.play("apple")
       ginny.play("peaches")
-      ginny.highest_scoring_word.must_equal "peaches"
+      ginny.highest_scoring_word.must_equal "PEACHES"
     end
   end
 
   describe "highest_word_score" do
     it "returns an integer" do
-    skip
-      ginny = Scrabble::Player.new
+    # skip
+      ginny = Scrabble::Player.new("ginny")
       ginny.play("apple")
       ginny.play("peaches")
       ginny.highest_word_score.must_be_kind_of Integer
     end
     it "returns the correct value" do
-    skip
-      ginny = Scrabble::Player.new
+    # skip
+      ginny = Scrabble::Player.new("ginny")
       ginny.play("apple")
       ginny.play("peaches")
       ginny.highest_word_score.must_equal 64

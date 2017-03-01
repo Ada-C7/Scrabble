d9fac112a30fdfbbd7c5ca9a49d6a8aad299034d
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-03-01 12:51:17 -0800

    changed where specs were being called from

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -27,37 +27,45 @@ before do  "Create an instance"
 
   end
 
-  describe "method tests Anna wrote" do
-    before do
+  describe "total_score method" do
+    it "returns the correct score" do
+      ginny = Scrabble::Player.new
+      ginny.play("apple")
+      ginny.play("apple")
+      ginny.total_score.must_equal 18
+    end
+  end
+
+  describe "highest_scoring_word" do
+    it "returns a string" do
       ginny = Scrabble::Player.new
       ginny.play("apple")
       ginny.play("peaches")
+      ginny.highest_scoring_word.must_be_kind_of String
     end
-    
-    describe "total_score method" do
-      it "returns the correct score" do
-        ginny.total_score.must_equal 73
-      end
-    end
-
-    describe "highest_scoring_word" do
-      it "returns a string" do
-        ginny.highest_scoring_word.must_be_kind_of String
-      end
-      it "returns the correct value" do
-        ginny.highest_scoring_word.must_equal "peaches"
-      end
-    end
-
-    describe "highest_word_score" do
-      it "returns an integer" do
-        ginny.highest_word_score.must_be_kind_of Integer
-      end
-      it "returns the correct value" do
-        ginny.highest_word_score.must_equal 64
-      end
+    it "returns the correct value" do
+      ginny = Scrabble::Player.new
+      ginny.play("apple")
+      ginny.play("peaches")
+      ginny.highest_scoring_word.must_equal "peaches"
     end
   end
+
+  describe "highest_word_score" do
+    it "returns an integer" do
+      ginny = Scrabble::Player.new
+      ginny.play("apple")
+      ginny.play("peaches")
+      ginny.highest_word_score.must_be_kind_of Integer
+    end
+    it "returns the correct value" do
+      ginny = Scrabble::Player.new
+      ginny.play("apple")
+      ginny.play("peaches")
+      ginny.highest_word_score.must_equal 64
+    end
+  end
+
 
 
 

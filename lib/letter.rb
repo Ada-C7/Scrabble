955f808b0c8b411e11c20e810b45d754e0c76819
janicewilson <janice.wilson@mac.com>
2017-03-01 11:30:50 -0800

    Prepping test environment.

--- lib/letter.rb
+++ lib/letter.rb
@@ -7,34 +7,15 @@ module Scrabble
           SCORING_CHART = { %w(A E I O U L N R S T) => 1, %w(D G) =>  2, %w(B C M P) =>  3, %w(F H  V W Y) =>  4, %w(K) =>  5, %w(J X) => 8, %w(Q Z) => 10 }
 
           def initialize(name)
-
-
               raise ArgumentError.new "That's not a letter" unless ("A".."Z").include?(name)
               @name = name
               @point = assign_points
-
           end
-
 
           def assign_points
-
                SCORING_CHART.each do | key, value |
-
                     return value if key.include?(@name)
-
                end
-
           end
      end
-
 end
-
-# l = Scrabble::Letter.new ("0")
-
-# l = Scrabble::Letter.new ("l")
-
-#
-# puts l.point.class
-#
-# puts l.point
-# j = Scrabble::Letter.new("j")
--- lib/score.rb
+++ lib/score.rb
@@ -14,46 +14,6 @@ module Scrabble
                score
           end
 
-
-          def self.highest_score_from(array)
-
-               comparison = array.map {| word | score(word)}
-               bucket = array.zip(comparison).to_h
-               #winner = bucket.max_by {|key, value| value}
-              #  bucket.values.sort
-               winner = 0
-               puts bucket.inspect
-              #  winning_words = []
-               bucket.each do |key, value|
-                 if value > winner
-                   winner = value
-                 end
-               end
-
-              #  bucket.each do |key, value|
-              #    winning_words << key(winner)
-              #  end
-               #
-              #  winning_words
-
-              winning_words = bucket.find_all { |key, value| value == winner }.to_h
-              winning_names = winning_words.keys
-              winning_names
-
-              if winning_names.length > 1
-                shortest = ""
-                winning_names.each do |word|
-                  if word.length == 7
-                    return word
-                  else
-                    return winning_names.min_by(&:length)
-                  end
-                end
-
-              end
-
-          end
-
           def self.highest_score_from(words)
                scores = words.map {| word | score(word)} # calculate scores of words
                composite = words.zip(scores).to_h # combine scores and words
@@ -63,49 +23,6 @@ module Scrabble
                winner = winners.min_by { | word | word.length} # tile tie-breaker
           end
 
-          #      winner = 0
-          #      puts bucket.inspect
-          #     #  winning_words = []
-          #      bucket.each do |key, value|
-          #        if value > winner
-          #          winner = value
-          #        end
-          #      end
-          #
-          #     #  bucket.each do |key, value|
-          #     #    winning_words << key(winner)
-          #     #  end
-          #      #
-          #     #  winning_words
-          #
-          #     winning_words = bucket.find_all { |key, value| value == winner }.to_h
-          #     winning_names = winning_words.keys
-          #     winning_names
-          #
-          #     if winning_names.length > 1
-          #       shortest = ""
-          #       winning_names.each do |word|
-          #         if word.length == 7
-          #           return word
-          #         else
-          #           return winning_names.min_by(&:length)
-          #         end
-          #       end
-          #     end
-
      end
+
 end
-
-array = %w(A letter means that a used all the tiles)
-# array = %w(cat echo tiles)
-sample = Scrabble::Scoring
-
-# puts sample.class
-# puts sample
-# puts sample.inspect
-puts sample.score("cat")
-
-array = %w(A letter means that pliyer pleyer a used all the tiles)
->>>>>>> b5fe89a52922bd634ebd4ffded779fb32a4be638
-
-puts sample = Scrabble::Scoring.highest_score_from(array)

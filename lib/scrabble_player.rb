bec0b93a85fa5eacfbb9a54ccb95704b82d149c7
Haby Randall <sow.haby@gmail.com>
2017-03-01 13:24:54 -0800

    Added def play(word) and related specs

--- lib/scrabble_player.rb
+++ lib/scrabble_player.rb
@@ -2,59 +2,67 @@ module Scrabble
 
   class Player
     attr_reader :name, :plays
+    attr_accessor :score
 
     def initialize(name)
-      #Raise an argument error if
-      #name is not string
+      raise ArgumentError.new("Name input must be a String") if name.class != String
       @name = name
       @plays = []
+      @score = 0
     end
 
     #plays return array of words played by player
-    def plays
-      return @plays
+    # def plays
+    #   return @plays
+    # end
+
+    #return true of player has > 100 points
+    def won?
+      return @score > 100 ? true : false
+      #maybe score needs to be instance variable?
     end
 
     def play(word)
-      #raise argument error if
-      #word is not string
+      raise ArgumentError.new("Word must be a string") if word.class != String
+
+      if won?
+        return false #Allows player to to play until win
+      else
+        @plays << word
+      end
+      return Scrabble::Scoring.score(word)
       #call METHOD won?
       #if won == true
       #return false
       #else, return score
       #call Scrabble:Scoring.score(word)
     end
-
-    #returns sum of score of played words
-    def total_score
-      #access @plays
-      #LOOP/ITERATE through @plays
-      #run each word in plays through
-      #Scrabble:Scoring.score(word) METHOD
-      #save the total somehow
-      #return the total score
-    end
-
-    #return true of player has > 100 points
-    def won?
-      #return @score > 100 ? true : false
-      #maybe score needs to be instance variable?
-    end
-
-    #returns the highest scoring played word
-    def highest_scoring_word
-      #access @plays
-      #Run array @plays through METHOD
-      #return Scrabble::Scoring.highest_score_from
-    end
-
-    #return the highest scoring word's
-    #word score
-    def highest_word_score
-      #run highest_scoring_word METHOD
-      #run that return through Scrabble:Scoring.score(word)
-      #return as local variable.
-    end
-
   end
+
+  #returns sum of score of played words
+  def total_score
+    #access @plays
+    #LOOP/ITERATE through @plays
+    #run each word in plays through
+    #Scrabble:Scoring.score(word) METHOD
+    #save the total somehow
+    #return the total score
+  end
+
+
+  #returns the highest scoring played word
+  def highest_scoring_word
+    #access @plays
+    #Run array @plays through METHOD
+    #return Scrabble::Scoring.highest_score_from
+  end
+
+  #return the highest scoring word's
+  #word score
+  def highest_word_score
+    #run highest_scoring_word METHOD
+    #run that return through Scrabble:Scoring.score(word)
+    #return as local variable.
+  end
+
 end
--- specs/scrabble_player_spec.rb
+++ specs/scrabble_player_spec.rb
@@ -1,2 +1,29 @@
 require_relative '../specs/spec_helper.rb'
 require_relative '../lib/scrabble_player'
+
+describe "Player Class" do
+
+  before do
+    @new_player = Scrabble::Player.new("Lynn")
+  end
+
+  describe "Scrabble::Player#initialize" do
+    it "Takes name to initialize " do
+      @new_player.must_respond_to :name
+    end
+    it "Scrabble::Player(name) must be a String" do
+      proc {
+        Scrabble::Player.new(123)}.must_raise ArgumentError
+      end
+
+      describe "Play" do
+        it "Word must be a string" do
+          proc { @new_player.play(123)}.must_raise ArgumentError
+        end
+      end
+      it "object.play(word) would return correct score"  do
+        @new_player.play("hi").must_equal 5
+
+      end
+    end
+  end

86a4935e414258c95fb520146129295bb44254ce
Sahana Murthy <s.murthy730.com>
2017-03-01 15:52:16 -0800

    Started the TileBag class

---
+++ lib/tilebag.rb
@@ -0,0 +1,27 @@
+require_relative 'scoring'
+require_relative 'player'
+module Scrabble
+  class TileBag
+
+    def initialize
+      @tile_bag = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C",
+                  "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E",
+                  "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H",
+                  "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L",
+                  "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O",
+                  "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R",
+                  "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T",
+                  "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"]
+    end
+
+    def draw_tiles(num)
+      @tile_bag.sample(num)
+
+    end
+
+    def tiles_remaining
+
+    end
+
+  end
+end

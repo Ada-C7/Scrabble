68c1cddc30452f031abbc02a06836d69de942c53
ashtn <ahstonehorton@gmail.com>
2017-03-01 12:18:30 -0800

    All specs for self.score class passed!

--- Specs/scoring_class_spec.rb
+++ Specs/scoring_class_spec.rb
@@ -17,33 +17,33 @@ describe "Score Method for Scoring Class" do
   end
 
   it "Handles if the word passed has a space (two words)" do
-    
+
     proc { Scrabble::Scoring::score("two words") }.must_raise ArgumentError
   end
 
   it "Handles if the word passed has cAmeLCase " do
-    skip
+
     Scrabble::Scoring::score("cAmeLCase").must_be :>, 10
   end
 
   it "Score Method outputs integer" do
-    skip
+
     puts Scrabble::Scoring::score(@word).must_be_instance_of Integer
   end
 
   it "Score method will total the value of each letter in the word" do
-    skip
+
     Scrabble::Scoring::score(@word).must_equal 10
 
   end
 
   it "Adds a 50 point bonus to a 7 letter word" do
-    skip
+
     Scrabble::Scoring::score(@word_7_letters).must_equal 73
   end
 
   it "adds 50 point bonus on words greater than 7 letters " do
-    skip
+    
     Scrabble::Scoring::score(@word_8_letters).must_equal 71
   end
 end

883e25ffa6066db30218bd37e5079ec00a1eff71
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-03-01 12:09:04 -0800

    more tests for play method, some tests for .won? method, started to write .won? method

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,26 @@
 module Scrabble
 
   class Player
-    attr_reader :name, :plays
+    attr_reader :name, :plays, :won
 
     def initialize(name)
       @name = name
       @plays = []
+      @won = false
     end
 
 
 def play(word)
-  @plays << word
+  if @won == false
+    @plays << word
+    # return
+  else
+    return false
+  end
+end
+
+def won?
+  @won ? true : false
 end
 
   end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -9,6 +9,7 @@ require_relative '../lib/player'
 #Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "Scrabble::Player" do
+
   it "Creates a new instance of Player class" do
     new_player = Scrabble::Player.new("Dan")
     new_player.must_be_instance_of Scrabble::Player
@@ -24,10 +25,39 @@ describe "Scrabble::Player" do
     new_player.plays.must_be_instance_of Array
   end
 
-  it "plays returns an array of words played" do
+  it "play(word) adds word to @plays" do
     new_player = Scrabble::Player.new("Dan")
     new_player.play("word")
     new_player.plays.must_include "word"
   end
+
+  it "plays returns an array of all words played" do
+    new_player = Scrabble::Player.new("Dan")
+    new_player.play("word")
+    new_player.play("play")
+    new_player.play("win")
+    new_player.plays.must_equal ["word", "play", "win"]
+  end
+
+  it "play(word) returns false if player has already won" do
+    new_player = Scrabble::Player.new("Dan")
+    new_player.play("zoology")
+    new_player.play("subject")
+    new_player.play("game").must_equal false
+  end
+
+  it "player.won? returns true if player has over 100 points." do
+    new_player = Scrabble::Player.new("Dan")
+    new_player.play("zoology")
+    new_player.play("subject")
+    new_player.won?.must_equal true
+  end
+
+  it "player.won? returns false if player has <= 100 points." do
+    new_player = Scrabble::Player.new("Dan")
+    new_player.play("zoology")
+    new_player.won?.must_equal false
+  end
+
 
 end

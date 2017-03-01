6c84132f62d3cd0a01212ce5231b1d726af58ce4
Danielle Birbal <birbalds@gmail.com>
2017-03-01 11:41:03 -0800

    Completed specs for player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -2,6 +2,7 @@ require_relative './scoring'
 
 module Scrabble
     class Player < Scoring
+        attr_accessor :word_scores
         attr_reader :name, :plays, :total_score, :highest_word_score
 
         def initialize(name)
@@ -9,30 +10,27 @@ module Scrabble
             @plays = []
             @word_scores = []
             @total_score = 0
-            @highest_word_score = 0
+            # @highest_word_score = 0
         end
 
         def play(word)
+            raise ArgumentError, 'Invalid input for word' unless word =~ /^[a-z]+$/i
             @plays << word
             if won?
-                false
+                return false
             else
                 @word_scores << score(word)
+                @highest_word_score = @word_scores.max
                 @total_score = @word_scores.reduce(:+)
-            end
-            @word_scores.last
-            @highest_word_score = @word_scores.max
+                return @word_scores.last
+              end
         end
 
         def highest_scoring_word
             highest_score_from(@plays)
         end
 
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        private
-
         def won?
-
             if @total_score >= 100
                 true
             else
@@ -52,11 +50,12 @@ end
 
 # test = Scrabble::Player.new('Tamago')
 #
-# puts test.name
-# puts test.play('word')
-# puts test.play('cow')
-# puts test.play('zizzl')
-# puts test.play('qizzl')
-# puts test.total_score
-# puts test.highest_scoring_word
+# test.name
+# test.play('word')
+# test.play('cow')
+# test.play('zizzl')
+# test.play('qizzl')
+# # puts test.total_score
+# # puts test.highest_scoring_word
 # puts test.highest_word_score
+# puts test.word_scores.max
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -24,13 +24,9 @@ describe Scrabble::Player do
         end
 
         it 'initializes @player_score holds 0' do
-            Scrabble::Player.new('Tofu').player_score.must_equal 0
+            Scrabble::Player.new('Tofu').total_score.must_equal 0
         end
     end
-
-    # sample words:
-    # ZIP 14 JAZZ 29 WHIZ 19 JERKY 19
-    # 7-letter: SQUEEZE 75 QUICKLY 75
 
     describe '#plays' do
         it 'returns .plays as an Array' do
@@ -48,13 +44,35 @@ describe Scrabble::Player do
             @new_player.play('whiz').must_be_kind_of Integer
         end
 
+        it 'returns the correct score of the word' do
+            @new_player.play('zaz').must_equal 21
+        end
+
+        it 'adds the input word to the plays array' do
+            @new_player.play('word')
+            @new_player.plays.must_include 'word'
+        end
+
         it 'returns the correct score of a 7-letter word' do
             @new_player.play('squeeze').must_equal 75
         end
 
-        it 'returns false if @won is true' do
+        it 'returns false if the player has already won' do
             @new_player.play('zzzzzzz')
-            @new_player.play('jazz').must_equal false
+            @new_player.play('word').must_equal false
+        end
+
+        it 'wont add to the score if the player has already won' do
+            @new_player.play('zzzzzzz')
+            @new_player.play('jazz')
+            @new_player.total_score.must_equal 120
+        end
+
+        it 'Raises an error if incorrect argument is given' do
+            proc { @new_player.play(7) }.must_raise ArgumentError
+            proc { @new_player.play }.must_raise ArgumentError
+            proc { @new_player.play('word!') }.must_raise ArgumentError
+            proc { @new_player.play('reallylongword') }.must_raise ArgumentError
         end
     end
 
@@ -63,34 +81,57 @@ describe Scrabble::Player do
             %w(zip sqeeze whiz jerky pig).each { |word| @new_player.play(word) }
             @new_player.total_score.must_be_kind_of Integer
         end
-        it 'returns sum of scores of played word' do
-            %w(zip sqeeze whiz).each { |word| @new_player.play(word) }
-            @new_player.total_score.must_equal 89
+        it 'returns sum of scores of played words' do
+            %w(z zzz zz zqaa).each { |word| @new_player.play(word) }
+            @new_player.total_score.must_equal 82
         end
     end
 
     describe '#won?' do
         it 'returns #won as a Boolean value' do
+            [true, false].must_include @new_player.won?
         end
         it 'returns true when the player has over 100 pts' do
+            @new_player.play('zzzzzzz')
+            @new_player.won?.must_equal true
         end
-        it 'returns true when the player has exactly 100 pts' do # edge case
+        it 'returns true when the player has exactly 100 pts' do
+            @new_player.play('zzzzz')
+            @new_player.play('qqqqq')
+            @new_player.won?.must_equal true
         end
     end
 
     describe '#highest_scoring_word?' do # returns word
-        it 'returns .highest_scoring_word(array) as a String' do
+        before do
+            @new_player.play('qqqqq')
+            @new_player.play('zaz')
+            @new_player.play('aaaaaa')
+        end
+        it 'returns .highest_scoring_word as a String' do
+            @new_player.highest_scoring_word.must_equal 'qqqqq'
         end
         it 'returns the first word for duplicate high scores' do
+            @new_player.play('zzzzz')
+            @new_player.highest_scoring_word.must_equal 'qqqqq'
         end
     end
 
     describe '#highest_word_score?' do # returns score
-        it 'returns .highest_word_score(array) as an Integer' do
+        before do
+            @new_player.play('qqqqq')
+            @new_player.play('zaz')
+            @new_player.play('aaaaaa')
+        end
+        it 'returns .highest_word_score as an Integer' do
+            @new_player.highest_word_score.must_be_instance_of Integer
         end
         it 'returns the highest scoring word from unique scores' do
+            @new_player.highest_word_score.must_equal 50
         end
         it 'returns one score from tied highest word scores' do
+            @new_player.play('zzzzz')
+            @new_player.highest_word_score.must_equal 50
         end
     end
 end

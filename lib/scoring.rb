4fbb687f2dcef62d5fbe6f541c86d6efa78060de
sai samant <saisamant@gmail.com>
2017-03-01 13:05:43 -0800

    changed filenames to class name

---
+++ lib/scoring.rb
@@ -0,0 +1,74 @@
+module Scrabble
+  class Scoring
+
+    def self.score(word)
+      #check if the word is a string of letters. if it isn't raise an Argument error
+      #iterate over the characters in the word and assign each letter a value from the score chart and add up the total
+      #if the string is 7 characters add 50 points to the total
+      #return total score
+
+      word = word.downcase
+
+      if word !~ /^[a-z]+$/
+        raise ArgumentError.new "Please use only letters"
+      end
+
+      score_chart = {"a" => 1, "b" => 3, "c" => 3, "d" => 2, "e" => 1, "f" => 4, "g" => 2, "h" => 4, "i" => 1, "j" => 8, "k" => 5, "l" => 1, "m" => 3, "n" => 1, "o" => 1, "p" => 3, "q" => 10, "r" => 1, "s" => 1, "t" => 1, "u" => 1, "v" => 4, "w" => 4, "x" => 8, "y" => 4, "z" => 10}
+
+      total = 0
+
+      word.each_char do |letter|
+        score_chart.each do |tile, value|
+          if letter == tile
+            total += value
+          end #end of if
+        end #end of do
+      end #end of do
+
+      if word.length == 7
+        total += 50
+      end
+
+      return total
+
+    end #end of score method
+
+    def self.highest_score_from(array_of_words)
+      raise ArgumentError "Input must be Array" if array_of_words.class != Array
+
+      raise ArgumentError "Must include at least one word" if array_of_words.length == 0
+
+        
+      scores = array_of_words.map { |word| Scoring.score(word) }
+
+      words_and_scores = array_of_words.zip(scores).to_h
+
+      #word with max score
+      max_score = words_and_scores.values.max
+
+      #hash of winning words + scores (all scores = max_score)
+      word_with_max_score = words_and_scores.select { |k, v| v == max_score }
+
+      #array of winning words
+      winning_words = word_with_max_score.keys
+
+      #length of shortest word in array of winning word
+      min_word_length = (winning_words.min_by { |word| word.length }).length
+
+      #tie-breaker conditionals
+      if winning_words.length == 1 #only one winner
+        return winning_words.join
+      elsif winning_words.any? { |word| word.length == 7 }
+        return array_of_words.find { |word| word.length == 7 && winning_words.include?(word) }
+      elsif (winning_words.select { |word| word.length == min_word_length}).length > 1 #more than one word of wining length
+        return array_of_words.find { |word| word.length == min_word_length && winning_words.include?(word) }
+
+      else #more than one winner, no 7-letters, only one of min length
+        return winning_words.min_by { |word| word.length }
+      end #end of tiebreaker if
+    end #end of def highest_score_from
+
+  end #end of class
+
+
+end #end of module
---
+++ specs/scoring_spec.rb
@@ -0,0 +1,92 @@
+require_relative 'spec_helper'
+require_relative '../lib/scoring'
+
+
+
+describe "Scoring" do
+
+  describe "score" do
+    before do
+      @score = Scrabble::Scoring.score("word")
+    end
+
+    it "Must return an integer" do
+      @score.must_be_instance_of Integer
+    end
+
+    it "Must receive String of letters as input" do
+      proc {
+        Scrabble::Scoring.score("5h7")
+      }.must_raise ArgumentError
+
+      proc {
+        Scrabble::Scoring.score("bats?")
+      }.must_raise ArgumentError
+
+      proc {
+        Scrabble::Scoring.score("Hello mom")
+      }.must_raise ArgumentError
+    end
+
+    it "7-letter words must get 50 more points" do
+      @score_check = Scrabble::Scoring.score("penguin")
+      @score_check.must_equal 60 #10 points plus 50 bonus
+
+      @score_check = Scrabble::Scoring.score("chicken")
+      @score_check.must_equal 68 #10 points plus 50 bonus
+    end
+
+    it "Must return correct score for word input" do
+      @score_check = Scrabble::Scoring.score("falcon")
+      @score_check.must_equal 11
+
+      @score_check = Scrabble::Scoring.score("jay")
+      @score_check.must_equal 13
+
+      @score_check = Scrabble::Scoring.score("quail")
+      @score_check.must_equal 14
+    end
+  end
+end
+
+describe "highest_score_from" do
+  before do
+    @array_of_words = %w(owl chicken duck emu kiwi)
+  end
+
+  it "must be an Array that has at least one word" do
+    proc {
+      Scrabble::Scoring.highest_score_from([]).must_raise ArgumentError.new
+    }
+  end
+
+  it "must be an Array" do
+    proc { Scrabble::Scoring.highest_score_from("word").must_raise ArgumentError.new }
+    proc { Scrabble::Scoring.highest_score_from(56).must_raise ArgumentError.new }
+    proc { Scrabble::Scoring.highest_score_from("").must_raise ArgumentError.new}
+  end
+
+  it "Returns the String with highest score" do
+    Scrabble::Scoring.highest_score_from(@array_of_words).must_equal "chicken"
+  end
+
+  it "Shorter word wins if scores are the same" do
+    short_words = %w(tins doe it to)
+    Scrabble::Scoring.highest_score_from(short_words).must_equal "doe"
+  end
+
+  it "7-letter word is the tie-breaker" do
+    seven = %w(aaaaaad zzzzzj)
+    Scrabble::Scoring.highest_score_from(seven).must_equal "aaaaaad"
+  end
+
+  it "returns first 7-letter word" do
+    more_sevens = %w(aaaaaad zzzzzj aaaaatg)
+    Scrabble::Scoring.highest_score_from(more_sevens).must_equal "aaaaaad"
+  end
+
+  it "First word wins if all highest-score words have same length" do
+    same = %w(so tins toes it to)
+    Scrabble::Scoring.highest_score_from(same).must_equal "tins"
+  end
+end

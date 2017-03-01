2c7f5f73f30be14f58d8278fc3f08f3b43af6499
Alix Hamilton <alix.hamilton@gmail.com>
2017-02-28 17:06:24 -0800

    refactored #highest_score_from in Scoring class

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,13 +1,15 @@
 module Scrabble
   class Scoring
 
-    SCORE_CHART = {"A"=>1, "B"=>3, "C"=>3, "D"=>2,
-    "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-    "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-    "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-    "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-    "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-    "Y"=>4, "Z"=>10}
+    SCORE_CHART = {
+      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+      "Y"=>4, "Z"=>10
+    }
 
     def self.score(word)
       raise ArgumentError.new("input must be string") if word.class != String
@@ -22,20 +24,15 @@ module Scrabble
 
     def self.highest_score_from(words)
 
-      scores = {}
-      words.each do |word|
-        scores[word] = score(word)
-      end
+      highest_score = words.map { |word| score(word) }.max
+      highest_words = words.select {|word| score(word) == highest_score }
 
-      highest_words = scores.select {|word, score| score == scores.values.max}
-      bonus_words = highest_words.select {|word, score| word.length == 7}
+      return highest_words.first if highest_words.length == 1
 
-      if highest_words.length == 1
-        return highest_words.keys.first
-      elsif bonus_words.length > 0
-        return bonus_words.keys.first
+      if highest_words.any? { |word| word.length == 7 }
+        highest_words.select {|word| word.length == 7}.first
       else
-        return highest_words.keys.min_by {|word| word.length}
+        highest_words.min_by {|word| word.length}
       end
 
     end

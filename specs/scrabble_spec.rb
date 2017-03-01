cbdd818ef2e6d5bd72caaf25313362fe6ccd95c7
Anacelia Gillette <acgillette426@gmail.com>
2017-03-01 11:17:20 -0800

    Started Wave 2

--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -1,14 +1,11 @@
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'minitest/skip_dsl'
 require_relative '../lib/scrabble'
 
 describe "Baseline" do
 
-
   it "Has a hash" do
     Scrabble::Scoring::SCORE_CHART.must_be_instance_of Hash
   end
+
 end
 
 

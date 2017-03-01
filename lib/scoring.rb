304b2594732a8aedaa13465efd1cd389e0a33d85
Cara Comfort <cara.comfort10@gmail.com>
2017-03-01 12:15:26 -0800

    Wrote score method. Started highest_score_from method

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,5 +1,3 @@
-require 'csv'
-
 module Scrabble
 
   class Scoring
@@ -11,18 +9,51 @@ module Scrabble
       'K' => 5,
       'J' => 8, 'X' => 8,
       'Q' => 10, 'Z' => 10 }
-      # attr_reader :score_chart
-      # def initialize
-      #   @score_chart = []
-      #   CSV.open("support/score_chart.csv").each do |line|
-      #     @score_chart << line[1..-1]
-      #   end
 
+      # Returns the score for a given word.
+      # Seven letter words get a 50+ bonus.
     def self.score(word)
-      4
+      raise ArgumentError.new("word must be a string") if word.class != String
+      raise ArgumentError.new("word must contain characters a-z") if !all_letters?(word)
+      raise ArgumentError.new("word must be at most 7 letters") if word.length > 7
+
+      letters = word.upcase.split("")
+      letter_values = letters.map {|letter| score = SCORE_CHART[letter]}
+      score = word.length < 7 ? letter_values.sum : letter_values.sum + 50
     end
 
+    # Returns the word with the highest score.
+    # If theres a tie, any seven letter words win,
+    # otherwise the shortest word wins.
     def self.highest_score_from(array_of_words)
+      # scores = words.group_by {|word| score(word)}
+      # top_word(s) = scores.max[1] # [10, ["pink", "purple"]]
+      # return top_word(s)[0] if top_word(s).length == 1
+      # there's a tie
+        #find the score of each word
+      # return the word with the highest score if there is no tie
+      # otherwise, return 1. 7 letter word 2. shortest word 3. first word
+
+      max_score = 0
+      best_word = ""
+      array_of_words.each do |word|
+        if score(word) > max
+          max_score = score(word)
+          best_word = word
+        elsif score(word) == max #if tie, winner would be shorter word.
+          if word.length < best_word.length
+            best_word = word
+          end
+        end
+      end
+      return best_word
+    end
+
+    private
+
+    # determine whether word contains only characters a-z
+    def self.all_letters?(word)
+      word[/[a-zA-Z]+/] == word
     end
   end
 end
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -6,11 +6,6 @@ require_relative '../lib/scoring.rb'
 
 describe "Scoring" do
   describe "score" do
-    # let (:scoring) {Scrabble::Scoring.new}
-    # it "Can be initialized" do
-    #   # scoring = Scrabble::Scoring.new
-    #   scoring.must_be_instance_of Scrabble::Scoring
-    # end
     it "returns an integer" do
       score = Scrabble::Scoring.score("a")
       score.must_be_instance_of Integer
@@ -33,7 +28,7 @@ describe "Scoring" do
     end
 
     it "adds 50 points to the total socre if the word length is 7 letters" do
-      Scrabble::Scoring.score("TRAVELS").must_equal 59
+      Scrabble::Scoring.score("TRAVELS").must_equal 60
     end
 
     it "raises an argument error if input is other than string of letters" do
@@ -102,29 +97,3 @@ describe "Scoring" do
     end
   end
 end
-
-# define_method(:scrabble_score) do
-# 		scrabble_word = self.split("")
-# 		scrabble_score = 0
-#
-# 		scrabble_word.each() do |letter|
-# 			case letter
-# 			when "a", "A", "e", "E", "i", "I", "o", "O", "u", "U", "l", "L", "n", "N", "r", "R", "s", "S", "t", "T"
-# 				scrabble_score = scrabble_score.+(1)
-# 			when "d", "D", "g", "G"
-# 				scrabble_score = scrabble_score.+(2)
-# 			when "b", "B", "c", "C", "m", "M", "p", "P"
-# 				scrabble_score = scrabble_score.+(3)
-# 			when "f", "F", "h", "H", "v", "V", "w", "W", "y", "Y"
-# 				scrabble_score = scrabble_score.+(4)
-# 			when "k", "K"
-# 				scrabble_score = scrabble_score.+(5)
-# 			when "j", "J", "x", "X"
-# 				scrabble_score = scrabble_score.+(8)
-# 			when "q", "Q", "z", "Z"
-# 				scrabble_score = scrabble_score.+(10)
-# 			else
-# 				scrabble_score
-# 			end
-# 		end
-# 		scrabble_score

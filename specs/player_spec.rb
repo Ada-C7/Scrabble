fae6f38c071a0b0be90faad957d00c0758702318
jjousun <jjousun@gmail.com>
2017-03-01 13:46:22 -0800

    Didn't save

---
+++ specs/player_spec.rb
@@ -0,0 +1,55 @@
+require_relative 'spec_helper'
+require_relative '../lib/player'
+
+describe 'Player' do
+  describe "name" do
+    it "returns the name" do
+
+    end
+  end
+
+  describe "plays" do
+    it "returns an Array of the words played" do
+
+    end
+
+  end
+
+  describe "play(word)" do
+    it "adds the input word to the plays Array" do
+
+    end
+
+    it "returns false if the player has already won" do
+
+    end
+
+    it "returns the score of the word" do
+
+    end
+  end
+
+  describe "total_score" do
+    it "returns the sum of scores of played words" do
+
+    end
+  end
+
+  describe "won?" do
+    it "returns true if player has more than 100 points, otherwise returns false" do
+
+    end
+  end
+
+  describe "highest_scoring_word" do
+    it "returns the highest scoring word" do
+
+    end
+  end
+
+  describe "highest_word_score" do
+    it "returns the highest_scoring_word score" do
+
+    end
+  end
+end 

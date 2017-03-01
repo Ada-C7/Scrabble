e234947b1b3daf883662c5fc4003ca5649e70ce1
Brenna <bcmdarroch@gmail.com>
2017-03-01 13:25:59 -0800

    fixed won? spec, built highest_scoring_word and highest_word_score tests, build those methods, passed all tests, wave 2 complete :)

--- lib/player.rb
+++ lib/player.rb
@@ -9,6 +9,7 @@ module Scrabble
       @total_score = 0 #total_score: Returns the sum of scores of played words
     end
 
+    #won?: If the player has over 100 points, returns true, otherwise returns false
     def won?
       @total_score >= 100 ? true : false
     end
@@ -16,19 +17,26 @@ module Scrabble
     def play(word)
       # Returns false if player has already won
       return false if won?
+
       score = Scrabble::Scoring.score(word)
       @total_score += score
+
       # play(word): Adds the input word to the plays Array
       @plays << word
+
       # Returns the score of the word
       return score
     end
 
-
-    #total_score: Returns the sum of scores of played words
-    #won?: If the player has over 100 points, returns true, otherwise returns false
     #highest_scoring_word: Returns the highest scoring played word
+    def highest_scoring_word
+      Scrabble::Scoring.highest_score_from(@plays)
+    end
+
     #highest_word_score: Returns the highest_scoring_word score
+    def highest_word_score
+      Scrabble::Scoring.score(highest_scoring_word)
+    end
 
   end
 end

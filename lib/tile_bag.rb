2298a42f8bbb58b80e06516bee3cc47ed6da9c2e
add2point71dots <alcland@gmail.com>
2017-03-01 16:34:16 -0800

    started draw_tiles method

--- lib/tile_bag.rb
+++ lib/tile_bag.rb
@@ -1,10 +1,19 @@
+
+
 module Scrabble
   class TileBag
-    attr_reader :tiles_left
+    attr_reader :tiles_remaining
 
     def initialize
-      @tiles_left = %W( A A A A A A A A A B B C C D D D D E E E E E E E E E E E E F F G G G H H I I I I I I I I I J K L L L L M M N N N N N N O O O O O O O O P P Q R R R R R R S S S S T T T T T T U U U U V V W W X Y Y Z )
+      @tiles_remaining = %W( A A A A A A A A A B B C C D D D D E E E E E E E E E E E E F F G G G H H I I I I I I I I I J K L L L L M M N N N N N N O O O O O O O O P P Q R R R R R R S S S S T T T T T T U U U U V V W W X Y Y Z )
+    end
 
+    def draw_tiles(num)    
+      tiles_drawn = []
+      @tiles_remaining.shuffle
+      num.times { tiles_drawn << @tiles_remaining.pop }
+
+      return tiles_drawn
     end
 
   end
--- specs/tile_bag_spec.rb
+++ specs/tile_bag_spec.rb
@@ -7,20 +7,28 @@ describe "Wave 3" do
   end
 
   describe "TileBag#initialize" do
-    it "tiles_left is an array" do
-      @new_bag.tiles_left.must_be_instance_of Array
+    it "tiles_remaining is an array" do
+      @new_bag.tiles_remaining.must_be_instance_of Array
     end
 
     it "initial bag has 98 tiles" do
-      @new_bag.tiles_left.length.must_equal 98
+      @new_bag.tiles_remaining.length.must_equal 98
     end
 
     it "initial bag has correct number of some letters" do
-      a_tiles = @new_bag.tiles_left.select { |tile| tile == "A" }
-      e_tiles = @new_bag.tiles_left.select { |tile| tile == "E" }
+      a_tiles = @new_bag.tiles_remaining.select { |tile| tile == "A" }
+      e_tiles = @new_bag.tiles_remaining.select { |tile| tile == "E" }
 
       a_tiles.length.must_equal 9
       e_tiles.length.must_equal 12
+    end
+
+    describe "TileBag#draw_tiles" do
+      it "returns a collection of random tiles" do
+        @new_bag.draw_tiles(5).must_be_instance_of Array
+        @new_bag.draw_tiles(5).length.must_equal 5
+      end
+
     end
 
   end

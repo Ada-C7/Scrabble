7c281928a297fecf7d5c86f279bbd5e2a7990595
Olivia <ssecretsharer@gmail.com>
2017-03-01 16:57:03 -0800

    as is of close on wednesday. added wave 3 mods, highest_scorings added, loosely

--- lib/player_class.rb
+++ lib/player_class.rb
@@ -14,18 +14,40 @@ module Scrabble
       word.score
       won?
       @plays << word
+    end
 
-      # end
+    def draw_tiles(tile_bag)
+    #WAVE 3 returns a collection of random tiles, removes the tiles from the default set
+    end
+
+    def tiles_remaining
+    #WAVE 3 tiles_remaining returns the number of tiles remaining in the bag
+    end
 
       def won?
-        if total_score <= 100
+        if @total_score <= 100
           false
         else
-          puts "Winner!"
+          raise ArgumentError.new puts "There is a clear winner! Great job, #{@name}!! Game over :("
         end
+      end
 
+
+      def highest_scoring_word(array_of_words)
+        return Scoring::highest_score_from(array_of_words)
+      end
+
+      def highest_word_score(array_of_words)
+        word = Scoring::highest_score_from(array_of_words)
+        return Scoring::score(word)
       end
 
 
     end#class end
   end #Mod end
+
+  #Modifications to `Player`
+  #  Create specs for (__minimum 2__) and add to the `Player` class the following     instance methods:
+  #
+  #     - `#tiles` a collection of letters that the player can play (max 7)
+  #     - `#draw_tiles(tile_bag)` fills tiles array until it has 7 letters from the given tile bag

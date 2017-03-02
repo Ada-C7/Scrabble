df1ada15bbe653d7a41ed5bc6aab421a1398b5fe
AlisonZ <alisonkzerbe@gmail.com>
2017-03-02 10:24:27 -0800

    initializes tilebag and makes tilebag from the letter scores

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -1,6 +1,27 @@
 module Scrabble
   class TileBag
+      attr_reader :tilebag
+      def initialize
+        @tilebag = []
+        @tile_counts = {
+              "a" => 9,"b" => 2,"c" => 2,"d" => 4,"e" => 12,"f" => 2,"g" => 3,
+              "h" => 2,"i" => 9,"j" => 1,"k" => 1,"l" => 4,"m" => 2,"n" => 6,
+              "o" => 8,"p" => 2,"q" => 1,"r" => 6,"s" => 4,"t" => 6,"u" => 4,
+              "v" => 2,"w" => 2,"x" => 1,"y" => 2,"z" => 1}
+        make_tilebag
+      end
 
-    
+
+
+      def make_tilebag
+        @tile_counts.each do |letter, count|
+            count.times do
+            @tilebag << letter
+            end
+        end
+
+
+      end
+
   end
 end
--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -2,29 +2,38 @@ require_relative 'spec_helper'
 
 require_relative '../lib/tilebag.rb'
 
+describe "Wave 3" do
+    before do
+        @my_tiles = Scrabble::TileBag.new
+    end
 
-describe "TileBag initialize" do
+    describe "TileBag initialize" do
 
-  it "has a collection of all default tiles"
-  end
+        it "Tilebag.new is an instance of tilebag" do
+            @my_tiles.must_be_instance_of Scrabble::TileBag
+        end
 
-end
+        it "has a collection of all default tiles" do
+            @my_tiles.tilebag.must_be_instance_of Array
+        end
 
-describe "draw_tiles(num) method for TileBag class" do
+    end
 
-  it "removes num items from the default tile collection "
-  end
+    xdescribe "draw_tiles(num) method for TileBag class" do
 
-  it "returns an array of the letters it removes" do
-  end
-end
+      it "removes num items from the default tile collection " do
+      end
 
-describe "tiles_remaining method" do
-  it "returns an integer that is the number of items remaining in default tile collection" do
-    # default_tiles size
-    # draw_tiles(7)
-    # tiles_remaining should be initial size - 7
-  end
-end
+      it "returns an array of the letters it removes" do
+      end
+    end
+
+    xdescribe "tiles_remaining method" do
+      it "returns an integer that is the number of items remaining in default tile collection" do
+        # default_tiles size
+        # draw_tiles(7)
+        # tiles_remaining should be initial size - 7
+      end
+    end
 
 end

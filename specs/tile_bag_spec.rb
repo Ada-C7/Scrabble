f544443045f4e8792154aa3f82dd4e9bccc080bd
tehut <tehut.m.g@gmail.com>
2017-03-01 16:32:22 -0800

    added tests for tile bag initialization

--- specs/tile_bag_spec.rb
+++ specs/tile_bag_spec.rb
@@ -6,3 +6,21 @@ require 'minitest/reporters'
 # require 'minitest/skip_dsl'
 require 'minitest/pride'
 require_relative '../lib/scoring'
+
+describe "Test Tile_Bag creation" do
+  it "instantiates and can be called" do
+    newbag = Scrabble::TileBag.new
+    newbag.class.must_be_kind_of Class
+  end
+
+  it "Tile_Bag class initializes with empty player hand []"do
+  newbag = Scrabble::TileBag.new
+  newbag.player_hand.length.must_equal 0
+  end
+
+  it "Tile_Bag class intializes with full bag " do
+    newbag = Scrabble::TileBag.new
+    newbag.tiles_remaining.must_equal #NUMBER!
+  end
+
+end

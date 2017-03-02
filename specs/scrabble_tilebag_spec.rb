1926184773e0c9b3b5c5c1555ceb7d64018b10a9
Hyunji Kim <ricecakemonster@gmail.com>
2017-03-01 16:22:36 -0800

    all tiles entered

--- specs/scrabble_tilebag_spec.rb
+++ specs/scrabble_tilebag_spec.rb
@@ -6,9 +6,15 @@ require 'minitest/skip_dsl'
 require_relative '../lib/scrabble_tilebag.rb'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-# describe "TileBag#initialize" do
-#   it "Creates a Scrabble::TileBag class" do
-#     tilebag = Scrabble::TileBag.new
-#     tilebag.must_be_kind_of Scrabble::TileBag
-#   end
-# end
+describe "TileBag#initialize" do
+  it "Creates a Scrabble::TileBag class" do
+    tilebag = Scrabble::TileBag.new
+    tilebag.must_be_kind_of Scrabble::TileBag
+  end
+
+  # it "Setting up a collection of all default tiles" do
+  #   tilebag = Scrabble::TileBag.new
+  #   tilebag.all_tiles.must_equal
+  #
+  # end
+end

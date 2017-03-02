cb1df31f2abab1ceedcb94fb98f37ab46cf52a88
Haby Randall <sow.haby@gmail.com>
2017-03-02 14:40:45 -0800

    Started TileBag class, writing draw_tiles method

--- lib/scrabble_tilebag.rb
+++ lib/scrabble_tilebag.rb
@@ -1,33 +1,35 @@
 module Scrabble
 
   class TileBag
+    attr_reader :bag_of_tiles
 
     def initialize
+
+      @bag_of_tiles = shuffle_tiles
       #sets up a collection of all default tiles
-      @tile_array = []
-      TILE_HASH.each do |letter, number|
-        number.times { @tile_array << letter}
-      end
-      @tile_array.shuffle!
+
     end
 
-TILE_HASH = {
-  "A" => 9, "N" => 6,
-  "B" => 2, "O" => 8,
-  "C" => 2, "P" => 2,
-  "D" => 4, "Q" => 1,
-  "E" => 12, "R" => 6,
-  "F" => 2, "S" => 4,
-  "G" => 3, "T" => 6,
-  "H" => 2, "U" => 4,
-  "I" => 9, "V" => 2,
-  "J" => 1, "W" => 2,
-  "K" => 1, "X" => 1,
-  "L" => 4, "Y" => 2,
-  "M" => 2, "Z" => 1
-}
+    TILE_HASH = {
+      "A" => 9, "N" => 6,
+      "B" => 2, "O" => 8,
+      "C" => 2, "P" => 2,
+      "D" => 4, "Q" => 1,
+      "E" => 12, "R" => 6,
+      "F" => 2, "S" => 4,
+      "G" => 3, "T" => 6,
+      "H" => 2, "U" => 4,
+      "I" => 9, "V" => 2,
+      "J" => 1, "W" => 2,
+      "K" => 1, "X" => 1,
+      "L" => 4, "Y" => 2,
+      "M" => 2, "Z" => 1
+    }
 
     def draw_tiles(num)
+      new_tiles = []
+      num.times {new_tiles << @bag_of_tiles.pop }
+      return new_tiles
       #returns a collection of num random tiles
       #updates set in tilebag instance.
       #also may eventually update tiles in player hand
@@ -38,6 +40,13 @@ TILE_HASH = {
       #accesses array and puts or prints it.
     end
 
+    def shuffle_tiles
+      tile_array = []
+      TILE_HASH.each do |letter, number|
+        number.times { tile_array << letter}
+      end
+      tile_array.shuffle!
+    end
   end
 
 end
--- specs/scrabble_tilebag_spec.rb
+++ specs/scrabble_tilebag_spec.rb
@@ -1,2 +1,31 @@
 require_relative '../specs/spec_helper.rb'
 require_relative '../lib/scrabble_tilebag'
+
+describe "TileBag class" do
+
+  before do
+    @tiles = Scrabble::TileBag.new
+  end
+
+  describe "Scrabble::TileBag#initialize" do
+    it "Responds to an instance of bag_of_tiles" do
+      @tiles.must_respond_to :bag_of_tiles
+    end
+    describe "draw_tiles" do
+      it "It returns an array of tiles from TileBag" do
+        new_tiles = @tiles.draw_tiles(4)
+        new_tiles.must_be_instance_of Array
+      end
+      it "Removes tiles from bag_of_tiles" do
+        first_draw = @tiles.bag_of_tiles.length
+        @tiles.draw_tiles(5)
+        second_draw = @tiles.bag_of_tiles.length
+
+        first_draw.wont_equal second_draw
+      end
+    end
+
+  end
+
+
+end

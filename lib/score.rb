7c904033847920e7aa74ed27dc24968b4fe370c1
Spatterjaaay <aja.pursova@gmail.com>
2017-03-01 16:21:46 -0800

    minor changes

--- lib/score.rb
+++ lib/score.rb
@@ -10,9 +10,9 @@ module Scrabble
                if word.class != String || word.length > 7 || word.length < 2
                     raise ArgumentError.new("Word must be a string between 2 and 7 characters long")
                end
-               puts word.inspect
+              #  puts word.inspect
                word = word.split("")
-               puts word.inspect
+              #  puts word.inspect
                word.map! {| letter | Letter.new(letter.upcase).point}
                score = word.inject { | sum, points | sum + points }
                score += 50 if word.length == 7

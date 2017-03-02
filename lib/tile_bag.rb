f89c6ef34b0682f1c70edc29cd19dfb2549180ce
tehut <tehut.m.g@gmail.com>
2017-03-01 16:58:11 -0800

    framed tile_bag.rb

--- lib/tile_bag.rb
+++ lib/tile_bag.rb
@@ -1,31 +1,48 @@
-require_relative 'tile_bag'
-def bag
-array = []
-9.times { array << "A"}
-2.times { array << "B"}
-2.times { array << "C"}
-4.times { array << "D"}
-12.times { array << "E"}
-2.times { array << "F"}
-3.times { array << "G"}
-2.times { array << "H"}
-9.times { array << "I"}
-1.times { array << "J"}
-1.times { array << "K"}
-4.times { array << "L"}
-2.times { array << "M"}
-6.times { array << "N"}
-8.times { array << "O"}
-2.times { array << "P"}
-1.times { array << "Q"}
-6.times { array << "R"}
-4.times { array << "S"}
-6.times { array << "T"}
-4.times { array << "U"}
-2.times { array << "V"}
-2.times { array << "W"}
-1.times { array << "X"}
-2.times { array << "Y"}
-1.times { array << "Z"}
-return array
+require_relative 'scoring'
+require_relative 'player'
+module Scrabble
+  class TileBag
+
+    def bag
+    array = []
+    9.times { array << "A"}
+    2.times { array << "B"}
+    2.times { array << "C"}
+    4.times { array << "D"}
+    12.times { array << "E"}
+    2.times { array << "F"}
+    3.times { array << "G"}
+    2.times { array << "H"}
+    9.times { array << "I"}
+    1.times { array << "J"}
+    1.times { array << "K"}
+    4.times { array << "L"}
+    2.times { array << "M"}
+    6.times { array << "N"}
+    8.times { array << "O"}
+    2.times { array << "P"}
+    1.times { array << "Q"}
+    6.times { array << "R"}
+    4.times { array << "S"}
+    6.times { array << "T"}
+    4.times { array << "U"}
+    2.times { array << "V"}
+    2.times { array << "W"}
+    1.times { array << "X"}
+    2.times { array << "Y"}
+    1.times { array << "Z"}
+    return array
+    end
+
+    def initialize
+    end
+
+    def draw_tiles
+    end
+
+    def tiles_remaining
+    end
+
+    end
+  end
 end

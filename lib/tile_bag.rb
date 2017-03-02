fbad23b0ec4c6c15ca6ff49c249f54f1e7aa02fe
Jackie Watanabe <sushiigirl@gmail.com>
2017-03-01 16:13:40 -0800

    initialized array tiles_left and created test to make sure tiles_left is an array and has the right number of tiles

--- lib/tile_bag.rb
+++ lib/tile_bag.rb
@@ -1,5 +1,11 @@
 module Scrabble
   class TileBag
-    
+    attr_reader :tiles_left
+
+    def initialize
+      @tiles_left = %W( A A A A A A A A A B B C C D D D D E E E E E E E E E E E E F F G G G H H I I I I I I I I I J K L L L L M M N N N N N N O O O O O O O O P P Q R R R R R R S S S S T T T T T T U U U U V V W W X Y Y Z )
+
+    end
+
   end
 end
--- specs/tile_bag_spec.rb
+++ specs/tile_bag_spec.rb
@@ -2,5 +2,20 @@ require_relative 'spec_helper'
 require_relative '../lib/tile_bag'
 
 describe "Wave 3" do
+  before do
+    @new_bag = Scrabble::TileBag.new
+  end
+
+  describe "TileBag#initialize" do
+    it "tiles_left is an array" do
+      @new_bag.tiles_left.must_be_instance_of Array
+    end
+
+    it "initial bag has 98 tiles" do
+      @new_bag.tiles_left.length.must_equal 98
+    end
+
+  end
+
 
 end

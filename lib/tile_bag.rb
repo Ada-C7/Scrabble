2846d11ffb17fc16cfbb41db7383d2195ac37c95
Jackie Watanabe <sushiigirl@gmail.com>
2017-03-01 16:59:20 -0800

    adjusted test to count tiles of some letters in the initial bag and working on tests to check for randomness of tiles

--- lib/tile_bag.rb
+++ lib/tile_bag.rb
@@ -8,7 +8,7 @@ module Scrabble
       @tiles_remaining = %W( A A A A A A A A A B B C C D D D D E E E E E E E E E E E E F F G G G H H I I I I I I I I I J K L L L L M M N N N N N N O O O O O O O O P P Q R R R R R R S S S S T T T T T T U U U U V V W W X Y Y Z )
     end
 
-    def draw_tiles(num)    
+    def draw_tiles(num)
       tiles_drawn = []
       @tiles_remaining.shuffle
       num.times { tiles_drawn << @tiles_remaining.pop }
--- specs/tile_bag_spec.rb
+++ specs/tile_bag_spec.rb
@@ -16,17 +16,43 @@ describe "Wave 3" do
     end
 
     it "initial bag has correct number of some letters" do
-      a_tiles = @new_bag.tiles_remaining.select { |tile| tile == "A" }
-      e_tiles = @new_bag.tiles_remaining.select { |tile| tile == "E" }
 
-      a_tiles.length.must_equal 9
-      e_tiles.length.must_equal 12
+      @new_bag.tiles_remaining.count { |tile| tile == "A" }.must_equal 9
+      @new_bag.tiles_remaining.count { |tile| tile == "E" }.must_equal 12
+
+      # a_tiles = @new_bag.tiles_remaining.select { |tile| tile == "A" }
+      # e_tiles = @new_bag.tiles_remaining.select { |tile| tile == "E" }
+      #
+      # a_tiles.length.must_equal 9
+      # e_tiles.length.must_equal 12
     end
 
     describe "TileBag#draw_tiles" do
       it "returns a collection of random tiles" do
         @new_bag.draw_tiles(5).must_be_instance_of Array
         @new_bag.draw_tiles(5).length.must_equal 5
+      end
+
+      it "removes the randomly selected tiles from tiles_remaining" do
+        skip
+        drawn_tiles = @new_bag.draw_tiles(4)
+
+        # tile1 = drawn_tiles[0]
+        # tile2 = drawn_tiles[1]
+        # tile3 = drawn_tiles[2]
+        # tile4 = drawn_tiles[3]
+        #
+
+
+        @new_bag.tiles_remaining.length.must_equal 94
+
+
+        #
+        # drawn_tiles = @new_bag.draw_tiles(7)
+        #
+        # @new_bag.tiles_remaining.length.must_equal 86
+
+
       end
 
     end

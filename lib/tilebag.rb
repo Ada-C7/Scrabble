a064da8b86df229a4db5d9f86d9ab2c8020e4bd8
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-03-02 15:51:25 -0800

    implemented draw_tiles method in TileBag class - ready to be tested

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -5,24 +5,24 @@ module Scrabble
     attr_reader :tile_bag
 
     def initialize
-      @tile_bag = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C",
+      @tile_bag = [ "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C",
                   "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E",
                   "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H",
                   "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L",
                   "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O",
                   "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R",
                   "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T",
-                  "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"]
+                  "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y",
+                  "Y", "Z" ].shuffle
     end
 
     def draw_tiles(num)
-      @tile_bag.sample(num)
-
+      raise ArgumentError unless num.class == Integer
+      @tile_bag.pop(num)
     end
 
     def tiles_remaining
-
+      @tile_bag.length
     end
-
   end
 end

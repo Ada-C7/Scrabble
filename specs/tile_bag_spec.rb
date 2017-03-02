80e85a247ee966e29df98f4812123ad4ff663d0c
tehut <tehut.m.g@gmail.com>
2017-03-01 16:46:46 -0800

    added test for remaining tiles method

--- specs/tile_bag_spec.rb
+++ specs/tile_bag_spec.rb
@@ -9,18 +9,32 @@ require_relative '../lib/scoring'
 
 describe "Test Tile_Bag creation" do
   it "instantiates and can be called" do
+    skip
     newbag = Scrabble::TileBag.new
     newbag.class.must_be_kind_of Class
   end
 
   it "Tile_Bag class initializes with empty player hand []"do
-  newbag = Scrabble::TileBag.new
-  newbag.player_hand.length.must_equal 0
+    skip
+    newbag = Scrabble::TileBag.new
+    newbag.player_hand.length.must_equal 0
   end
 
   it "Tile_Bag class intializes with full bag " do
+    skip
     newbag = Scrabble::TileBag.new
-    newbag.tiles_remaining.must_equal #NUMBER!
+    newbag.tiles_remaining.must_equal 98
+  end
+end
+
+
+describe "Test tiles_remaining method" do
+  it "tiles_remaining returns array that is 98 - 7 tiles long" do
+    newbag = Scrabble::TileBag.new
+    starting = newbag.tiles_remaining
+    newbag.draw_tiles
+    newbag.tiles_remaining.must_equal starting-7
+
   end
 
 end

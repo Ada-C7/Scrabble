e8dbb3b963952b63a1218ac7d60a43a8ab389a80
Spatterjaaay <aja.pursova@gmail.com>
2017-03-03 14:08:06 -0800

    populating tilebag

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -6,17 +6,40 @@ require 'awesome_print'
 module Scrabble
 
   class TileBag
+    attr_reader :tile
 
     def initialize
       @tile = %w(
-      A A A A A A A A
+      A A A A A A A A A
       B B
       C C
       D D D D
       E E E E E E E E E E E E
       F F
+      G G G
+      H H
+      I I I I I I I I I
+      J
+      K
+      L L L L
+      M M
+      N N N N N N
+      O O O O O O O O
+      P P
+      Q
+      R R R R R R
+      S S S S
+      T T T T T T
+      U U U U
+      V V
+      W W
+      X
+      Y Y
+      Z
       )
     end
 
   end
 end
+bag = Scrabble::TileBag.new
+puts bag.tile.length

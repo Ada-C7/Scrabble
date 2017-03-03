39b28efc32f3d075248c8a80ccf36926a6518a6e
Bo Trethewey <botrethewey@gmail.com>
2017-03-03 12:31:34 -0800

    Added tests for tilebag class

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -5,27 +5,61 @@ describe "Scrabble tilebag" do
   before do
     @tilebag = Scrabble::Tilebag.new
   end
+
   describe "#initialize method" do
     it "Should have a colection of default tiles" do
       @tilebag.tiles_remaining.wont_be_empty
       @tilebag.tiles_remaining.length.must_equal 98
     end
+
+    it "Should have appropriate number of tiles per each alphabet" do
+      @tilebag.tiles.count("A").must_equal 9
+      @tilebag.tiles.count("B").must_equal 2
+      @tilebag.tiles.count("C").must_equal 2
+      @tilebag.tiles.count("D").must_equal 4
+      @tilebag.tiles.count("E").must_equal 12
+      @tilebag.tiles.count("F").must_equal 2
+      @tilebag.tiles.count("G").must_equal 3
+      @tilebag.tiles.count("H").must_equal 2
+      @tilebag.tiles.count("I").must_equal 9
+      @tilebag.tiles.count("J").must_equal 1
+      @tilebag.tiles.count("K").must_equal 1
+      @tilebag.tiles.count("L").must_equal 4
+      @tilebag.tiles.count("M").must_equal 2
+      @tilebag.tiles.count("N").must_equal 6
+      @tilebag.tiles.count("O").must_equal 8
+      @tilebag.tiles.count("P").must_equal 2
+      @tilebag.tiles.count("Q").must_equal 1
+      @tilebag.tiles.count("R").must_equal 6
+      @tilebag.tiles.count("S").must_equal 4
+      @tilebag.tiles.count("T").must_equal 6
+      @tilebag.tiles.count("U").must_equal 4
+      @tilebag.tiles.count("V").must_equal 2
+      @tilebag.tiles.count("W").must_equal 2
+      @tilebag.tiles.count("X").must_equal 1
+      @tilebag.tiles.count("Y").must_equal 2
+      @tilebag.tiles.count("Z").must_equal 1
+    end
   end
+
   describe "#draw_tiles method" do
     it "Should return a collection of random tiles" do
       @tilebag.draw_tiles(1).must_be_instance_of Array
       @tilebag.draw_tiles(3).length.must_equal 3
     end
+
     it "Should remove the tiles from the default set" do
       @tilebag.draw_tiles(3)
       @tilebag.tiles_remaining.must_equal 95
     end
   end
+
   describe "tiles_remaining method" do
     it "Returns the number of tiles remaining in the bag" do
       @tilebag.draw_tiles(7)
       @tilebag.tiles_remaining.must_equal 91
-
+      @tilebag.draw_tiles(91)
+      @tilebag.tiles_remaining.must_equal 0
     end
   end
 end

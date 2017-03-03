4e1f33f297a9833d2ce9f34b0b691f072672e502
add2point71dots <alcland@gmail.com>
2017-03-03 14:41:28 -0800

    Added tests for randomness (as best as we could)

--- specs/tile_bag_spec.rb
+++ specs/tile_bag_spec.rb
@@ -15,16 +15,9 @@ describe "TileBag tests" do
       @new_bag.tiles_in_bag.length.must_equal 98
     end
 
-    it "initial bag has correct number of some letters" do
-
+    it "initial bag has correct number of As and Es" do
       @new_bag.tiles_in_bag.count { |tile| tile == "A" }.must_equal 9
       @new_bag.tiles_in_bag.count { |tile| tile == "E" }.must_equal 12
-
-      # a_tiles = @new_bag.tiles_in_bag.select { |tile| tile == "A" }
-      # e_tiles = @new_bag.tiles_in_bag.select { |tile| tile == "E" }
-      #
-      # a_tiles.length.must_equal 9
-      # e_tiles.length.must_equal 12
     end
 
     describe "TileBag#draw_tiles(num)" do
@@ -57,33 +50,36 @@ describe "TileBag tests" do
         (leftover_tiles + drawn_tiles).sort.must_equal original_tiles
       end
 
-      it "checks random stuff" do skip
-        drawn_tiles = @new_bag.draw_tiles(4)
+      it "drawing tiles from different tilebags (usually) returns different tile arrays" do
+        original_drawn_tiles = @new_bag.draw_tiles(7)
+        new_drawn_tiles = []
+        50.times { new_drawn_tiles << Scrabble::TileBag.new.draw_tiles(7) }
 
-        # tile1 = drawn_tiles[0]
-        # tile2 = drawn_tiles[1]
-        # tile3 = drawn_tiles[2]
-        # tile4 = drawn_tiles[3]
-        #
-        @new_bag.tiles_in_bag.length.must_equal 94
+        new_drawn_tiles.each_with_index do |tiles, i|
+          new_drawn_tiles[i] = (tiles == original_drawn_tiles ? "match" : "diff")
+        end
 
-        #
-        # drawn_tiles = @new_bag.draw_tiles(7)
-        #
-        # @new_bag.tiles_in_bag.length.must_equal 86
+        new_drawn_tiles.must_include "diff"
       end
 
+      it "drawing tiles from the same tilebag will return (at least some) different tile arrays" do
+        original_drawn_tiles = @new_bag.draw_tiles(7)
+        new_drawn_tiles = []
+        13.times { new_drawn_tiles << @new_bag.draw_tiles(7)}
+
+        new_drawn_tiles.each_with_index do |tiles, i|
+          new_drawn_tiles[i] = (tiles == original_drawn_tiles ? "match" : "diff")
+        end
+
+        new_drawn_tiles.must_include "diff"
+      end
     end
 
     describe "TileBag#tiles_remaining" do
       it "returns the number of tiles left in bag" do
         @new_bag.draw_tiles(3)
-
         @new_bag.tiles_remaining.must_equal 95
       end
     end
-
   end
-
-
 end

e0b4b0b135808da2b601a1c561754ddcca33b120
jjousun <jjousun@gmail.com>
2017-03-02 20:51:35 -0800

    trying to find the extra 'end' but I can't, but now I'm scared to not commit the change :(

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -20,33 +20,33 @@ describe TileBag do
 
       letters = @tile_bag.collection.group_by { |letter| letter[0] }
       letter_hash = letters.each do |k, v| letters[k] = v.length
-        
+
+
+      end
+
+    end
+  end
+    describe "draw_tiles" do
+
+
+      it "takes an Integer" do
+        proc { @tile_bag.draw_tiles("cat")}.must_raise ArgumentError
+      end
+
+      it "returns an Array of same length as number of tiles drawn" do
+        @tile_bag.draw_tiles(4).must_be_instance_of Array
+        @tile_bag.draw_tiles(4).length.must_equal 4
+      end
+
+      it "returns an Array of one-character length strings" do
+        draw = @tile_bag.draw_tiles(3)
+        draw.
+      end
+
+    end
+
+    describe "tiles_remaining" do
 
     end
 
   end
-
-  describe "draw_tiles" do
-
-
-    it "takes an Integer" do
-      proc { @tile_bag.draw_tiles("cat")}.must_raise ArgumentError
-    end
-
-    it "returns an Array of same length as number of tiles drawn" do
-      @tile_bag.draw_tiles(4).must_be_instance_of Array
-      @tile_bag.draw_tiles(4).length.must_equal 4
-    end
-
-    it "returns an Array of one-character length strings" do
-      draw = @tile_bag.draw_tiles(3)
-      draw.
-    end
-
-  end
-
-  describe "tiles_remaining" do
-
-  end
-
-end

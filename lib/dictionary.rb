30035aac3ad0291f5346d015b43b0c4aa2ab66a7
Alix Hamilton <alix.hamilton@gmail.com>
2017-03-02 22:39:02 -0800

    changed dictionary to class methods and variables only

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,13 +1,22 @@
 module Scrabble
   class Dictionary
-    attr_reader :valid_words
 
-    def initialize
-      @valid_words = File.read("support/dictionary.txt").split("\n").group_by { |word| word.length }
+    def self.valid_words
+      @@valid_words ||= File.read("support/dictionary.txt")
+        .split("\n").group_by { |word| word.length }
     end
 
-    def valid_word?(word)
-      @valid_words[word.length].include? word.upcase
+    def self.valid_word?(word)
+
+      if !valid_words.keys.include?(word.length) ||
+        !valid_words[word.length].include?(word.upcase)
+        
+        puts "Word not found in Scrabble Dictionary."
+        false
+      else
+        true
+      end
     end
+
   end
 end

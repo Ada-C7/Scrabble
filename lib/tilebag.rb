dc1e4f66fec0eccf666683362075d891f0d99ad7
jjousun <jjousun@gmail.com>
2017-03-02 20:52:06 -0800

    Finished methods for tilebag, I think it works :)

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -1,22 +1,28 @@
 module Scrabble
   class TileBag
+    attr_reader :collection
 
+    #initialize Should set up the instance with a collection of all default tiles
     def initialize
       @collection = %w(e)*12 + %w(a i)*9 + %w(o)*8 + %w(n r t)*6 + %w(d l s u)*4 + %w(g)*3 + %w(b c f h m p v w y)*2 + %w(j k q x z)
     end
 
-    def self.draw_tiles(num)
-      return @collection.rand(0..num)
+    #draw_tiles(num) returns a collection of random tiles, removes the tiles from the default set
+    def draw_tiles(num)
+      raise ArgumentError.new "Specify only numbers" if num.class != Integer
+      player_hand = []
+
+      num.times do |letter|
+        player_hand << @collection.shuffle!.pop
+      end
+
+      return player_hand
     end
 
-    def self.tiles_remaining
-
+    #tiles_remaining returns the number of tiles remaining in the bag
+    def tiles_remaining
+      return @collection
     end
 
   end
 end
-#initialize Should set up the instance with a collection of all default tiles
-#draw_tiles(num) returns a collection of random tiles, removes the tiles from the default set
-#tiles_remaining returns the number of tiles remaining in the bag
-
-print Scrabble::TileBag.draw_tiles(20)

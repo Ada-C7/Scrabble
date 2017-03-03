a6a168a89a6e176d24d41b29a79ccddfa281edf0
Danielle Birbal <birbalds@gmail.com>
2017-03-03 13:51:53 -0800

    Added testing for Tilebag

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -2,11 +2,13 @@
 module Scrabble
     class TileBag
         attr_accessor :tiles_drawn, :in_bag
+
         TILES = {
             A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9,
             J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6,
             S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
         }.freeze
+
         def initialize
             @in_bag = [] # tiles in bag
             TILES.each do |key, values| # create all tiles and put in bag
@@ -17,13 +19,17 @@ module Scrabble
         end
 
         def draw_tiles(number)
-            raise ArgumentError 'not enough tiles' if tiles_remaining < number
+            raise ArgumentError, 'not enough tiles' if @in_bag.length < number
             @tiles_drawn = @in_bag.pop(number)
-            @tiles_drawn # <-- TEST PURPOSES ONLY
+            @tiles_drawn
         end
 
         def tiles_remaining
-            @in_bag.length
+            if @in_bag.empty?
+                puts 'There are no more tiles!'
+            else
+                @in_bag.length
+          end
         end
   end
 end
@@ -32,7 +38,7 @@ end
 # test_one = Scrabble::TileBag.new
 #
 # puts "remaining tiles: #{test_one.tiles_remaining}"
-# test_one.draw_tiles(70)
+# test_one.draw_tiles(99)
 # puts "remaining tiles: #{test_one.tiles_remaining}"
 # test_one.draw_tiles(20)
 # puts "remaining tiles: #{test_one.tiles_remaining}"
--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -6,23 +6,57 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/tilebag'
 
-describe 'tilebag#initialize' do
-  it 'creates a bag of tiles' do
-  end
-end
+describe Scrabble::TileBag do
+    before do
+        @new_tilebag = Scrabble::TileBag.new
+    end
 
-describe 'tilebag#draw_tiles(num)' do
-  it 'removes the correct number of tiles from set' do
-  end
-  it 'removes the correct letters from the set' do
-  end
-end
+    describe 'tilebag#initialize' do
+        it 'creates a bag of tiles' do
+            @new_tilebag.in_bag.wont_be_empty
+        end
 
-describe 'tilebag#tiles_remaining' do
-  it 'returns an Integer' do
-  end
-  it 'returns correct number of tiles' do
-  end
-  it 'throws a message when 0 tiles' do
-  end
+        it 'tile bag contains the correct number of tiles' do
+            @new_tilebag.in_bag.length.must_equal 98
+        end
+
+        it 'tile bag has correct number of individual letters' do
+            @new_tilebag.in_bag.find_all { |letter| letter == 'A' }.length.must_equal 9
+            @new_tilebag.in_bag.find_all { |letter| letter == 'H' }.length.must_equal 2
+            @new_tilebag.in_bag.find_all { |letter| letter == 'T' }.length.must_equal 6
+        end
+    end
+
+    describe 'tilebag#draw_tiles(num)' do
+        it 'removes the correct number of tiles from set' do
+            @new_tilebag.draw_tiles(7)
+            @new_tilebag.in_bag.length.must_equal 91
+        end
+
+        it 'removes the correct letters from the set' do
+            @new_tilebag.draw_tiles(3)
+            @new_tilebag.in_bag.wont_include @new_tilebag.tiles_drawn
+        end
+
+        it 'raises an error if user tries to draw more tiles than available' do
+            proc { @new_tilebag.draw_tiles(99) }.must_raise ArgumentError
+        end
+    end
+
+    describe 'tilebag#tiles_remaining' do
+        it 'returns an Integer' do
+            @new_tilebag.tiles_remaining.must_be_instance_of Integer
+        end
+
+        it 'returns correct number of tiles' do
+            @new_tilebag.tiles_remaining.must_equal 98
+            @new_tilebag.draw_tiles(8)
+            @new_tilebag.tiles_remaining.must_equal 90
+        end
+
+        it 'throws a message when 0 tiles' do
+            @new_tilebag.draw_tiles(98)
+            proc { @new_tilebag.tiles_remaining }.must_output /.+/
+        end
+    end
 end
